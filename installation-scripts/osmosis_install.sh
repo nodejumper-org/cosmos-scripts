8cab1494cd51c7707701c79becbdd9592b8bd588
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-04-21 11:51:29 +0300

    Fix Osmosis installation script

--- installation-scripts/osmosis_install.sh
+++ installation-scripts/osmosis_install.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 sudo apt update && sudo apt upgrade -y
 
 version="1.17.2" \
@@ -14,11 +12,12 @@ version="1.17.2" \
 go version # go version go1.17.2 linux/amd64
 
 sudo apt install -y make gcc jq
+sudo apt install snapd -y && sudo snap install lz4
 
 cd && git clone https://github.com/osmosis-labs/osmosis
-cd osmosis && git checkout v7.0.3 && make install
+cd osmosis && git checkout v7.0.4 && make install
 
-osmosisd version # 7.0.3
+osmosisd version # v7.0.4
 
 # replace nodejumper with your own moniker, if you'd like
 osmosisd init "${1:-nodejumper}" --chain-id osmosis-1
@@ -28,12 +27,15 @@ mv -f genesis.json ~/.osmosisd/config/genesis.json
 jq -S -c -M '' ~/.osmosisd/config/genesis.json | shasum -a 256 # 23fe76392e7535eafb73f6d60f08538b2f35272454b4598b734b4ecb6f5a7c5e  -
 
 sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "0.0001uosmo"/g' ~/.osmosisd/config/app.toml
-sed -i 's/^snapshot-interval *=.*/snapshot-interval = 0/g' ~/.osmosisd/config/app.toml
+seeds="21d7539792ee2e0d650b199bf742c56ae0cf499e@162.55.132.230:2000,295b417f995073d09ff4c6c141bd138a7f7b5922@65.21.141.212:2000,ec4d3571bf709ab78df61716e47b5ac03d077a1a@65.108.43.26:2000,4cb8e1e089bdf44741b32638591944dc15b7cce3@65.108.73.18:2000,f515a8599b40f0e84dfad935ba414674ab11a668@osmosis.blockpane.com:26656,6bcdbcfd5d2c6ba58460f10dbcfde58278212833@osmosis.artifact-staking.io:26656"
+peers="83c06bc290b6dffe05aa9cec720bedfc118afcbc@rpc2.nodejumper.io:35656"
+sed -i -e "s/^seeds *=.*/seeds = \"$seeds\"/; s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.osmosisd/config/config.toml
 
 # in case of pruning
 sed -i 's/pruning = "default"/pruning = "custom"/g' ~/.osmosisd/config/app.toml
 sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "100"/g' ~/.osmosisd/config/app.toml
 sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' ~/.osmosisd/config/app.toml
+sed -i 's/snapshot-interval *=.*/snapshot-interval = 0/g' ~/.osmosisd/config/app.toml
 
 sudo tee <<EOF >/dev/null /etc/systemd/system/osmosisd.service
 [Unit]
@@ -51,26 +53,10 @@ EOF
 
 osmosisd unsafe-reset-all
 
-SNAP_RPC="http://rpc2.nodejumper.io:35657"
+rm -rf ~/.osmosisd/data && cd ~/.osmosisd
 
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
-
-sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" ~/.osmosisd/config/config.toml
+SNAP_NAME=$(curl -s https://snapshots2.nodejumper.io/osmosis/ | egrep -o ">osmosis-1.*\.tar.lz4" | tr -d ">")
+wget -O - https://snapshots2.nodejumper.io/osmosis/${SNAP_NAME} | lz4 -dc - | tar -xf -
 
 sudo systemctl daemon-reload && sudo systemctl enable osmosisd \
 && sudo systemctl restart osmosisd && sudo journalctl -u osmosisd -f --no-hostname -o cat
-
-
-# fix state sync issue (https://github.com/cosmos/cosmos-sdk/issues/10791) do it after the node is synchronized
-git clone https://github.com/tendermint/tendermint
-cd tendermint
-git checkout callum/app-version
-make install
-tendermint set-app-version 1 --home ~/.osmosisd
