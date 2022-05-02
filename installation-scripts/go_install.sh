c2a32d96ae3c29a008d00bd48f5bdef580662091
libra422 <libra.validator@gmail.com>
2022-05-03 01:41:29 +0700

    add version option

--- installation-scripts/go_install.sh
+++ installation-scripts/go_install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-version=${1:-"1.18.1"}
+while getopts v: flag; do
+  case "${flag}" in
+  v) VER=$OPTARG ;;
+  *) echo "WARN: unknown parameter: ${OPTARG}"
+  esac
+done
+
+version=${VER:-"1.18.1"}
+
 cd && wget "https://golang.org/dl/go$version.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
