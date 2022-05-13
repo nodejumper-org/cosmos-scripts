fc7f4a48c48f7eff8d1d7dd8a327e50ad8d27de2
libra422 <libra.validator@gmail.com>
2022-05-13 23:37:50 +0700

    rename variable and fix typo

--- node-config/node_installer.sh
+++ node-config/node_installer.sh
@@ -13,7 +13,7 @@ function configureNode {
   ### general
   local installationScriptUrl=$(jq -r '.installationScript' <<< "$config")
   local serviceName=$(jq -r '.serviceName' <<< "$config")
-  local chainHomeDir="$HOME/$(jq -r '.chainHomeDir' <<< "$config")"
+  local chainHomePath="$HOME/$(jq -r '.chainHomeDir' <<< "$config")"
   local stateSyncMode=$(jq -r '.stateSyncMode' <<<" $config")
   local prometheusIp=$(jq -r '.prometheusIp' <<<" $config")
 
@@ -46,58 +46,58 @@ function configureNode {
   fi
 
   ### client.toml
-  if [ -f "$chainHomeDir/config/client.toml" ]; then
-    sed -i 's/chain-id = ""/chain-id = "'$chainId'"/g' "$chainHomeDir/config/client.toml"
-    sed -i 's|node = "tcp:\/\/localhost:26657"|node = "tcp:\/\/localhost:'$portRpc'"|g' "$chainHomeDir/config/client.toml"
+  if [ -f "$chainHomePath/config/client.toml" ]; then
+    sed -i 's/chain-id = ""/chain-id = "'$chainId'"/g' "$chainHomePath/config/client.toml"
+    sed -i 's|node = "tcp:\/\/localhost:26657"|node = "tcp:\/\/localhost:'$portRpc'"|g' "$chainHomePath/config/client.toml"
   fi
 
   ### app.toml
-  sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "'$minGasPrice'"/g' "$chainHomeDir/config/app.toml"
-  sed -i 's/address = "0.0.0.0:9090"/address = "0.0.0.0:'$portGrpc'"/g' "$chainHomeDir/config/app.toml"
-  sed -i 's/address = "0.0.0.0:9091"/address = "0.0.0.0:'$portGrpcWeb'"/g' "$chainHomeDir/config/app.toml"
+  sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "'$minGasPrice'"/g' "$chainHomePath/config/app.toml"
+  sed -i 's/address = "0.0.0.0:9090"/address = "0.0.0.0:'$portGrpc'"/g' "$chainHomePath/config/app.toml"
+  sed -i 's/address = "0.0.0.0:9091"/address = "0.0.0.0:'$portGrpcWeb'"/g' "$chainHomePath/config/app.toml"
 
   if [ "$stateSyncMode" == "true" ]; then
     # set pruning to custom/100/2000/10
-    sed -i 's/pruning = "default"/pruning = "custom"/g' "$chainHomeDir/config/app.toml"
-    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "100"/g' "$chainHomeDir/config/app.toml"
-    sed -i 's/pruning-keep-every = "0"/pruning-keep-recent = "2000"/g' "$chainHomeDir/config/app.toml"
-    sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' "$chainHomeDir/config/app.toml"
-    sed -i 's/snapshot-interval = 0/snapshot-interval = 2000/g' "$chainHomeDir/config/app.toml"
+    sed -i 's/pruning = "default"/pruning = "custom"/g' "$chainHomePath/config/app.toml"
+    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "100"/g' "$chainHomePath/config/app.toml"
+    sed -i 's/pruning-keep-every = "0"/pruning-keep-every = "2000"/g' "$chainHomePath/config/app.toml"
+    sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' "$chainHomePath/config/app.toml"
+    sed -i 's/snapshot-interval = 0/snapshot-interval = 2000/g' "$chainHomePath/config/app.toml"
   else
     # set pruning to custom/100/0/10
-    sed -i 's/pruning = "default"/pruning = "custom"/g' "$chainHomeDir/config/app.toml"
-    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "100"/g' "$chainHomeDir/config/app.toml"
-    sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' "$chainHomeDir/config/app.toml"
+    sed -i 's/pruning = "default"/pruning = "custom"/g' "$chainHomePath/config/app.toml"
+    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "100"/g' "$chainHomePath/config/app.toml"
+    sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' "$chainHomePath/config/app.toml"
   fi
 
   ### config.toml
-  sed -i 's|laddr = "tcp:\/\/0.0.0.0:26656"|laddr = "tcp:\/\/0.0.0.0:'$portP2P'"|g' "$chainHomeDir/config/config.toml"
-  sed -i 's|pprof_laddr = "localhost:6060"|pprof_laddr = "localhost:'$portPprof'"|g' "$chainHomeDir/config/config.toml"
-  sed -i 's|indexer = "kv"|indexer = "'$indexer'"|g' "$chainHomeDir/config/config.toml"
-  sed -i 's|prometheus = false|prometheus = true|g' "$chainHomeDir/config/config.toml"
-  sed -i 's|prometheus_listen_addr = ":26660"|prometheus_listen_addr = ":'$portPrometheus'"|g' "$chainHomeDir/config/config.toml"
-  sed -i 's|proxy_app = "tcp:\/\/127.0.0.1:26658\"|proxy_app = "tcp:\/\/127.0.0.1:'$portProxyApp'"|g' "$chainHomeDir/config/config.toml"
+  sed -i 's|laddr = "tcp:\/\/0.0.0.0:26656"|laddr = "tcp:\/\/0.0.0.0:'$portP2P'"|g' "$chainHomePath/config/config.toml"
+  sed -i 's|pprof_laddr = "localhost:6060"|pprof_laddr = "localhost:'$portPprof'"|g' "$chainHomePath/config/config.toml"
+  sed -i 's|indexer = "kv"|indexer = "'$indexer'"|g' "$chainHomePath/config/config.toml"
+  sed -i 's|prometheus = false|prometheus = true|g' "$chainHomePath/config/config.toml"
+  sed -i 's|prometheus_listen_addr = ":26660"|prometheus_listen_addr = ":'$portPrometheus'"|g' "$chainHomePath/config/config.toml"
+  sed -i 's|proxy_app = "tcp:\/\/127.0.0.1:26658\"|proxy_app = "tcp:\/\/127.0.0.1:'$portProxyApp'"|g' "$chainHomePath/config/config.toml"
 
   if [ -n "$tlsCertPath" ] && [ -n "$tlsKeyPath" ] && [ "$tlsCertPath" != "null" ] && [ "$tlsKeyPath" != "null" ]; then
-    sed -i 's|tls_cert_file = ""|tls_cert_file = "'"$tlsCertPath"'"|g' "$chainHomeDir/config/config.toml"
-    sed -i 's|tls_key_file = ""|tls_key_file = "'"$tlsKeyPath"'"|g' "$chainHomeDir/config/config.toml"
+    sed -i 's|tls_cert_file = ""|tls_cert_file = "'"$tlsCertPath"'"|g' "$chainHomePath/config/config.toml"
+    sed -i 's|tls_key_file = ""|tls_key_file = "'"$tlsKeyPath"'"|g' "$chainHomePath/config/config.toml"
   fi
 
   if [ -n "$seeds" ]; then
-    sed -i -e "s/^seeds *=.*/seeds = \"$seeds\"/" "$chainHomeDir/config/config.toml"
+    sed -i -e "s/^seeds *=.*/seeds = \"$seeds\"/" "$chainHomePath/config/config.toml"
   fi
 
   if [ -n "$peers" ]; then
-    sed -i -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" "$chainHomeDir/config/config.toml"
+    sed -i -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" "$chainHomePath/config/config.toml"
   fi
 
   if [ "$stateSyncMode" == "true" ]; then
-    sed -i 's|laddr = "tcp:\/\/127.0.0.1:26657"|laddr = "tcp:\/\/0.0.0.0:'$portRpc'"|g' "$chainHomeDir/config/config.toml"
-    sed -i 's|cors_allowed_origins = \[\]|cors_allowed_origins = \["*"\]|g' "$chainHomeDir/config/config.toml"
-    sed -i 's|max_num_inbound_peers = 40|max_num_inbound_peers = 400|g' "$chainHomeDir/config/config.toml"
-    sed -i 's|max_num_outbound_peers = 10|max_num_outbound_peers = 100|g' "$chainHomeDir/config/config.toml"
+    sed -i 's|laddr = "tcp:\/\/127.0.0.1:26657"|laddr = "tcp:\/\/0.0.0.0:'$portRpc'"|g' "$chainHomePath/config/config.toml"
+    sed -i 's|cors_allowed_origins = \[\]|cors_allowed_origins = \["*"\]|g' "$chainHomePath/config/config.toml"
+    sed -i 's|max_num_inbound_peers = 40|max_num_inbound_peers = 400|g' "$chainHomePath/config/config.toml"
+    sed -i 's|max_num_outbound_peers = 10|max_num_outbound_peers = 100|g' "$chainHomePath/config/config.toml"
   else
-    sed -i 's|laddr = "tcp:\/\/127.0.0.1:26657"|laddr = "tcp:\/\/127.0.0.1:'$portRpc'"|g' "$chainHomeDir/config/config.toml"
+    sed -i 's|laddr = "tcp:\/\/127.0.0.1:26657"|laddr = "tcp:\/\/127.0.0.1:'$portRpc'"|g' "$chainHomePath/config/config.toml"
   fi
 
   ### UFW
