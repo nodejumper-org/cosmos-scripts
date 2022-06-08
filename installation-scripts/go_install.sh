8ad19042909fbcf4dbdade152a2cd96d878d0b85
libra422 <libra.validator@gmail.com>
2022-06-08 13:24:45 +0700

    don't cd to home

--- installation-scripts/go_install.sh
+++ installation-scripts/go_install.sh
@@ -11,10 +11,9 @@ done
 
 version=${VER:-"1.18.3"}
 
-cd || return
 curl -L -# -O "https://golang.org/dl/go$version.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
 rm "go$version.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-echo "export GOPATH=$HOME/go" >> ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
