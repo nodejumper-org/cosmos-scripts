1cd76801e47291c25a123e0c6bcad52e3fdcca99
libra422 <libra.validator@gmail.com>
2022-06-09 13:37:13 +0700

    upgrade version of sei testnet

--- installation-scripts/sei-testnet_install.sh
+++ installation-scripts/sei-testnet_install.sh
@@ -16,31 +16,29 @@ cd || return
 rm -rf sei-chain
 git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain || return
-git checkout 1.0.0beta
-go build -o build/seid ./cmd/sei-chaind
-mkdir -p $HOME/go/bin
-mv build/seid $HOME/go/bin
+git checkout 1.0.2beta
+make install
 seid version
 
 # replace nodejumper with your own moniker, if you'd like
-seid config chain-id sei-testnet-1
-seid init "${1:-nodejumper}" --chain-id sei-testnet-1
+seid config chain-id sei-testnet-2
+seid init "${1:-nodejumper}" --chain-id sei-testnet-2 -o
 
-curl https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/genesis.json > $HOME/.sei-chain/config/genesis.json
-sha256sum $HOME/.sei-chain/config/genesis.json # d212a915dcde84f1dc2208ca5ee890adfd6ffc5d4ff9a32332f50659b3b5ab1a
+curl https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-2/genesis.json > $HOME/.sei/config/genesis.json
+sha256sum $HOME/.sei/config/genesis.json # aec481191276a4c5ada2c3b86ac6c8aad0cea5c4aa6440314470a2217520e2cc
 
-curl https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/addrbook.json > $HOME/.sei-chain/config/addrbook.json
-sha256sum $HOME/.sei-chain/config/addrbook.json # 2ff327d2ab89c9ec56f86c14fdc67cbfc12e4716ae8cecc3bb497d92c4d8411e
+curl https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-2/addrbook.json > $HOME/.sei/config/addrbook.json
+sha256sum $HOME/.sei/config/addrbook.json # 9058b83fca36c2c09fb2b7c04293382084df0960b4565090c21b65188816ffa6
 
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001usei"|g' $HOME/.sei-chain/config/app.toml
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001usei"|g' $HOME/.sei/config/app.toml
 seeds=""
 peers="f36276cafd833d805f48ee0c3214b1d1b2f2193e@rpc1-testnet.nodejumper.io:28656"
-sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.sei-chain/config/config.toml
+sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.sei/config/config.toml
 
 # in case of pruning
-sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/.sei-chain/config/app.toml
-sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.sei-chain/config/app.toml
-sed -i 's|pruning-interval = "0"|pruning-interval = "10"|g' $HOME/.sei-chain/config/app.toml
+sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/.sei/config/app.toml
+sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.sei/config/app.toml
+sed -i 's|pruning-interval = "0"|pruning-interval = "10"|g' $HOME/.sei/config/app.toml
 
 sudo tee /etc/systemd/system/seid.service > /dev/null << EOF
 [Unit]
@@ -57,10 +55,10 @@ WantedBy=multi-user.target
 EOF
 
 seid unsafe-reset-all
-rm -rf $HOME/.sei-chain/data
-cd $HOME/.sei-chain || return
+rm -rf $HOME/.sei/data
+cd $HOME/.sei || return
 
-SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/sei-testnet/ | egrep -o ">sei-testnet-1.*\.tar.lz4" | tr -d ">")
+SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/sei-testnet/ | egrep -o ">sei-testnet-2.*\.tar.lz4" | tr -d ">")
 echo "Downloading a snapshot..."
 curl -# https://snapshots1-testnet.nodejumper.io/sei-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf -
 
