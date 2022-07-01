7326e9ca753515fc0eb887fdb9036abe754ddaf3
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-07-01 11:47:35 +0400

    Update peers

--- installation-scripts/kujira-testnet_install.sh
+++ installation-scripts/kujira-testnet_install.sh
@@ -31,7 +31,7 @@ sha256sum $HOME/.kujira/config/addrbook.json # 620fe1d5caf6544d61ea887c0c84664a1
 
 sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001ukuji"|g' $HOME/.kujira/config/app.toml
 seeds=""
-peers="eaa7e55efc03f23c5f461f71c06d444693d5352b@rpc1-testnet.nodejumper.io:29656,f7d58626a2d5a68476258caceee0e51d540b0522@147.182.252.151:26656,cf3f51d39ab28458f9d5fdc02d06c1e6fd5d9d68@159.223.10.122:26656,fb34b987aa87499862d78e23b05b977b04257b91@159.89.96.122:26656,447aa13452281b3f57c5c75ad14c602cf4d28e0f@178.62.199.206:26656,3290cd2702e36efa65a27ac777e072f46227aa3e@62.171.184.255:26656,85b24eb243b6e6bcae2438b9dc58bd2425eb843d@34.162.149.43:26656,2b01b6fb5f2accce46aa48f4c73472b1830e907c@18.130.28.227:26656,2bdde092ad699ad1254b134a500448a387625a58@207.154.212.21:26656"
+peers="eaa7e55efc03f23c5f461f71c06d444693d5352b@rpc1-testnet.nodejumper.io:29656"
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.kujira/config/config.toml
 
 # in case of pruning
--- installation-scripts/quicksilver-testnet_install.sh
+++ installation-scripts/quicksilver-testnet_install.sh
@@ -28,7 +28,7 @@ sha256sum $HOME/.quicksilverd/config/genesis.json # 3510dd3310e3a127507a513b3e9c
 
 sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001uqck"|g' $HOME/.quicksilverd/config/app.toml
 seeds="dd3460ec11f78b4a7c4336f22a356fe00805ab64@seed.killerqueen-1.quicksilver.zone:26656,8603d0778bfe0a8d2f8eaa860dcdc5eb85b55982@seed02.killerqueen-1.quicksilver.zone:27676"
-peers="c5cbd164de9c20a13e54e949b63bcae4052a948c@138.201.139.175:20956,9428068507466b542cbf378d59b77746c1d19a34@157.90.35.151:26657,4e7a6d8a3c8eeaad4be4898d8ec3af1cef92e28d@93.186.200.248:26656,eaeb462547cf76c3588e458120097b51db732b14@194.163.155.84:26656,51af5b6b4b0f5b2b53df98ec1b029743973f08aa@75.119.145.20:26656,9a9ed14d71a88354b0383419432ecce70e8cd2b3@161.97.152.215:26656,43bca26cb1b2e7474a8ffa560f210494023d5de4@135.181.140.225:26657,1ba34e984db33e075f10b4139a514e9ff7c07d7d@65.21.131.215:26676,86b0be64be9637f6e23ecf475edd164fa07b5665@65.21.134.202:26676"
+peers="4742e1b942acf17c31794cce80d199886d172c4f@rpc1-testnet.nodejumper.io:31656"
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.quicksilverd/config/config.toml
 sed -i 's|^indexer *=.*|indexer = "null"|g' $HOME/.quicksilverd/config/config.toml
 
