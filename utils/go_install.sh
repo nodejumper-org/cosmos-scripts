11d010621231c07af6b9acc7cc87b78aa6a22a91
libra422 <libra.validator@gmail.com>
2022-07-29 22:06:06 +0700

    update go install script

--- utils/go_install.sh
+++ utils/go_install.sh
@@ -13,5 +13,10 @@ curl -L -# -O "https://golang.org/dl/go$version.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
 rm "go$version.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
+
+source $HOME/.bash_profile
+PATH_INCLUDES_GO=$(grep "$HOME/go/bin" $HOME/.bash_profile)
+if [ -z "$PATH_INCLUDES_GO" ]; then
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+  echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
+fi
