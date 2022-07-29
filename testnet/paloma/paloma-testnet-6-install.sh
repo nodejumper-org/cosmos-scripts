8e9174d10c123407112195657c2f8ce12ea47456
libra422 <libra.validator@gmail.com>
2022-07-29 10:04:29 +0700

    update paloma

--- testnet/paloma/paloma-testnet-6-install.sh
+++ testnet/paloma/paloma-testnet-6-install.sh
@@ -24,23 +24,23 @@ printCyan "4. Building binaries..." && sleep 1
 cd || return
 curl -L https://github.com/CosmWasm/wasmvm/raw/main/api/libwasmvm.x86_64.so > libwasmvm.x86_64.so
 sudo mv -f libwasmvm.x86_64.so /usr/lib/libwasmvm.x86_64.so
-curl -L https://github.com/palomachain/paloma/releases/download/v0.3.0-alpha/paloma_0.3.0-alpha_Linux_x86_64.tar.gz > paloma.tar.gz
+curl -L https://github.com/palomachain/paloma/releases/download/v0.4.0-alpha/paloma_0.4.0-alpha_Linux_x86_64.tar.gz > paloma.tar.gz
 tar -xvzf paloma.tar.gz
 rm -rf paloma.tar.gz
 sudo mv -f palomad /usr/local/bin/palomad
-palomad version # v0.3.0-prealpha
+palomad version # v0.4.0-alpha
 
 palomad config chain-id $CHAIN_ID
 palomad init $NODE_MONIKER --chain-id $CHAIN_ID
 
 curl https://raw.githubusercontent.com/palomachain/testnet/master/paloma-testnet-6/genesis.json > $HOME/.paloma/config/genesis.json
-sha256sum $HOME/.paloma/config/genesis.json # bad4a385822e25d8357c6b0b6ba4f4ab7c47355b105d02397994f718fb9a407a
+sha256sum $HOME/.paloma/config/genesis.json # ad6d37a6ee49f0d04cb84150337968db6092ce1d2c10b5719eff52d73a12e34f
 
-curl https://raw.githubusercontent.com/palomachain/testnet/master/paloma-testnet-5/addrbook.json > $HOME/.paloma/config/addrbook.json
+curl https://raw.githubusercontent.com/palomachain/testnet/master/paloma-testnet-6/addrbook.json > $HOME/.paloma/config/addrbook.json
 
 sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001ugrain"|g' $HOME/.paloma/config/app.toml
 seeds=""
-peers="484e0d3cc02ba868d4ad68ec44caf89dd14d1845@paloma-testnet.nodejumper.io:33656"
+peers="484e0d3cc02ba868d4ad68ec44caf89dd14d1845@paloma-testnet.nodejumper.io:33656,8fab1d100d1c01de299788758d31d36c321c23b5@144.202.103.140:26656,8fab1d100d1c01de299788758d31d36c321c23b5@144.202.103.140:26656,1f74adfbfa794a53b104368fcb1189eddc18d66f@173.255.229.106:26656"
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.paloma/config/config.toml
 
 # in case of pruning
@@ -65,19 +65,19 @@ LimitNOFILE=10000
 WantedBy=multi-user.target
 EOF
 
-palomad tendermint unsafe-reset-all --home $HOME/.paloma --keep-addr-book
+palomad tendermint unsafe-reset-all --home $HOME/.paloma
 
-SNAP_RPC="https://paloma-testnet.nodejumper.io:443"
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
-
-sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.paloma/config/config.toml
+#SNAP_RPC="https://paloma-testnet.nodejumper.io:443"
+#LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+#BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+#TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+#
+#echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+#
+#sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+#s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+#s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+#s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.paloma/config/config.toml
 
 sudo systemctl daemon-reload
 sudo systemctl enable palomad
