49224993f4f0846652846615ac7cb65ebd96dd32
libra422 <libra.validator@gmail.com>
2022-07-29 21:07:01 +0700

    update common scripts

--- utils/common.sh
+++ utils/common.sh
@@ -1,14 +1,19 @@
-NC="\e[0m"            # no color
-CYAN="\e[1m\e[1;96m"  # cyan color
+NC="\e[0m"           # no color
+CYAN="\e[1m\e[1;96m" # cyan color
 
 function printLogo {
-    bash <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/logo.sh)
+  bash <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/logo.sh)
 }
 
 function printLine {
-    echo "=================================================================================================="
+  echo "=================================================================================================="
 }
 
 function printCyan {
-    echo -e "${CYAN}${1}${NC}"
+  echo -e "${CYAN}${1}${NC}"
+}
+
+function addToPath {
+  # shellcheck disable=SC2086
+  echo "export PATH=${PATH}:${1}" >>${HOME}/.bash_profile
 }
