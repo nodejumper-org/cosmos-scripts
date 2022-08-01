38fe5676b693f5025a289c421e176651b273a711
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-08-01 11:17:14 +0400

    Uncomment state sync

--- testnet/paloma/paloma-testnet-6-install.sh
+++ testnet/paloma/paloma-testnet-6-install.sh
@@ -67,17 +67,17 @@ EOF
 
 palomad tendermint unsafe-reset-all --home $HOME/.paloma
 
-#SNAP_RPC="https://paloma-testnet.nodejumper.io:443"
-#LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-#BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
-#TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-#
-#echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
-#
-#sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-#s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-#s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-#s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.paloma/config/config.toml
+SNAP_RPC="https://paloma-testnet.nodejumper.io:443"
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.paloma/config/config.toml
 
 sudo systemctl daemon-reload
 sudo systemctl enable palomad
