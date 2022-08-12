58f311ddd9a11465e0bb097a934d713d392dbcc5
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-08-12 15:34:04 +0400

    Update automatic upgrade scripts

--- stride/upgrade/v0.3.1/upgrade_automatic.sh
+++ stride/upgrade/v0.3.1/upgrade_automatic.sh
@@ -12,14 +12,14 @@ CHEAT_SHEET="https://nodejumper.io/stride-testnet/cheat-sheet"
 printCyan "Your node will be upgraded to version: $VERSION on block height: $BLOCK" && sleep 1
 
 for (( ; ; )); do
-  height=$($BINARY status |& jq -r ."SyncInfo"."latest_block_height")
+  height=$($BINARY status 2>&1 | jq -r .SyncInfo.latest_block_height)
   if ((height >= $BLOCK)); then
     source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/stride/upgrade/v0.3.1/upgrade_manual.sh)
     printCyan "Your node was successfully upgraded to version: $VERSION" && sleep 1
     strided version --long | head
     break
   else
-    printCyan "Current block height: $height"
+    echo "Current block height: $height"
   fi
   sleep 5
 done
