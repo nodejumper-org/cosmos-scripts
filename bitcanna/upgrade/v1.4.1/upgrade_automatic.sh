d53f19ef74922a302c2e905e372d82fe908913bf
vbloher <vbloher7@gmail.com>
2022-08-19 13:28:40 +0700

    fix bitcanna upgrade script

--- bitcanna/upgrade/v1.4.1/upgrade_automatic.sh
+++ bitcanna/upgrade/v1.4.1/upgrade_automatic.sh
@@ -14,9 +14,9 @@ printCyan "Your node will be upgraded to version: $VERSION on block height: $BLO
 for (( ; ; )); do
   height=$($BINARY status 2>&1 | jq -r .SyncInfo.latest_block_height)
   if ((height >= $BLOCK)); then
-    source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/main/bitcanna/upgrade/v1.4.1/upgrade_manual.sh)
+    source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/main/bitcanna/upgrade/$VERSION/upgrade_manual.sh)
     printCyan "Your node was successfully upgraded to version: $VERSION" && sleep 1
-    strided version --long | head
+    bitcanna version --long | head
     break
   else
     echo "Current block height: $height"
