fd2e5888fcd699c4c10945300b1b7d79defd2ac8
vbloher <vbloher7@gmail.com>
2022-08-19 13:41:52 +0700

    add unified auto upgrade script

---
+++ utils/chain_auto_upgrade.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/common.sh)
+
+while getopts n:t:v:b:c: flag; do
+  case "${flag}" in
+  n) CHAIN_NAME=$OPTARG ;;
+  t) TARGET_BLOCK=$OPTARG ;;
+  v) VERSION=$OPTARG ;;
+  b) BINARY=$OPTARG ;;
+  c) CHEAT_SHEET=$OPTARG ;;
+  *) echo "WARN: unknown parameter: ${OPTARG}"
+  esac
+done
+
+printLogo
+
+printCyan "Your $CHAIN_NAME node will be upgraded to version: $VERSION on block height: $TARGET_BLOCK" && sleep 1
+
+for (( ; ; )); do
+  height=$($BINARY status 2>&1 | jq -r .SyncInfo.latest_block_height)
+  if ((height >= TARGET_BLOCK)); then
+    bash <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/main/$CHAIN_NAME/upgrade/$VERSION/upgrade_manual.sh)
+    printCyan "Your node was successfully upgraded to version: $VERSION" && sleep 1
+    strided version --long | head
+    break
+  else
+    echo "Current block height: $height"
+  fi
+  sleep 5
+done
+
+printLine
+printCyan "Check logs:            sudo journalctl -u $BINARY -f --no-hostname -o cat"
+printCyan "Check synchronization: $BINARY status 2>&1 | jq .SyncInfo.catching_up"
+printCyan "More commands:         $CHEAT_SHEET"
