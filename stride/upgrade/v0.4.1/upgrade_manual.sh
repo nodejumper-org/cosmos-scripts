8a8bf97155d68c9e0f5d1b229be26a7abc9758e7
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-08-23 22:10:43 +0400

    Update upgrade script for stride v0.4.1

--- stride/upgrade/v0.4.1/upgrade_manual.sh
+++ stride/upgrade/v0.4.1/upgrade_manual.sh
@@ -1,9 +1,7 @@
 sudo systemctl stop strided
 
-cd || return
-rm -rf stride
-git clone https://github.com/Stride-Labs/stride.git
-cd stride || return
+cd $HOME/stride || return
+git fetch
 git checkout 90859d68d39b53333c303809ee0765add2e59dab
 make build
 sudo cp build/strided $(which strided)
