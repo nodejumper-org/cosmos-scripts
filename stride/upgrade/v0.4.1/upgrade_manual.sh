6b205d40f01b93dc1402da6dc6d7f707c2cea430
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-08-23 22:07:44 +0400

    Update upgrade script for stride v0.4.1

--- stride/upgrade/v0.4.1/upgrade_manual.sh
+++ stride/upgrade/v0.4.1/upgrade_manual.sh
@@ -6,7 +6,6 @@ git clone https://github.com/Stride-Labs/stride.git
 cd stride || return
 git checkout 90859d68d39b53333c303809ee0765add2e59dab
 make build
-mkdir -p $HOME/go/bin
-sudo cp $HOME/stride/build/strided $HOME/go/bin
+sudo cp build/strided $(which strided)
 
 sudo systemctl restart strided
