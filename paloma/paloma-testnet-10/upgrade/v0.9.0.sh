2f1396948ac5c65f69921aef44b5d251e0f3f466
vbloher <vbloher7@gmail.com>
2022-09-27 12:20:38 +0700

    fix typo

--- paloma/paloma-testnet-10/upgrade/v0.9.0.sh
+++ paloma/paloma-testnet-10/upgrade/v0.9.0.sh
@@ -13,7 +13,7 @@ curl -L https://github.com/palomachain/pigeon/releases/download/v0.8.0/pigeon_Li
 tar -xvzf pigeon.tar.gz
 rm -rf pigeon.tar.gz
 sudo mv -f pigeon /usr/local/bin/pigeon
-palomad version # v0.8.0
+pigeon version # v0.8.0
 
 sudo systemctl start palomad
 sudo systemctl start pigeond
