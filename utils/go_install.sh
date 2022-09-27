fcda1a5deec53efb59f381d5417accb5f2343059
vbloher <vbloher7@gmail.com>
2022-09-27 14:18:01 +0700

    update go version to 1.19.1

--- utils/go_install.sh
+++ utils/go_install.sh
@@ -7,7 +7,7 @@ while getopts v: flag; do
   esac
 done
 
-version=${VER:-"1.18.3"}
+version=${VER:-"1.19.1"}
 
 curl -L -# -O "https://golang.org/dl/go$version.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
