d1c18a451459d316aec0d7b4f0f0866d454fff6b
Hdmiimdh <uladzislau.kuzmin@gmail.com>
2022-09-29 14:49:42 +0400

    Update sei scripts

--- sei/atlantic-1/install.sh
+++ sei/atlantic-1/install.sh
@@ -26,16 +26,16 @@ cd || return
 rm -rf sei-chain
 git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain || return
-git checkout 1.1.2beta-internal
+git checkout tags/1.2.0beta
 make install
-seid version #1.1.2beta-internal
+seid version #1.2.0beta
 
 seid config keyring-backend test
 seid config chain-id $CHAIN_ID
 seid init $NODE_MONIKER --chain-id $CHAIN_ID
 
 curl -s https://raw.githubusercontent.com/sei-protocol/testnet/main/sei-incentivized-testnet/genesis.json > $HOME/.sei/config/genesis.json
-sha256sum $HOME/.sei/config/genesis.json #
+sha256sum $HOME/.sei/config/genesis.json #4ae7193446b53d78bb77cab1693a6ddf6c1fe58c9693ed151e71f43956fdb3f7
 
 curl -s https://raw.githubusercontent.com/sei-protocol/testnet/main/sei-incentivized-testnet/addrbook.json > $HOME/.sei/config/addrbook.json
 
@@ -66,6 +66,18 @@ WantedBy=multi-user.target
 EOF
 
 seid tendermint unsafe-reset-all --home $HOME/.sei --keep-addr-book
+
+SNAP_RPC="https://sei-testnet.nodejumper.io:443"
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.sei/config/config.toml
 
 sudo systemctl daemon-reload
 sudo systemctl enable seid
