b6f75eaa1c7f5f9b145d3a8570c589f70a794746
vbloher <vbloher7@gmail.com>
2022-10-18 22:28:43 +0700

    add sei upgrade

--- sei/atlantic-1/upgrade/1.2.3beta.sh
+++ sei/atlantic-1/upgrade/1.2.3beta.sh
@@ -4,7 +4,7 @@ cd || return
 rm -rf sei-chain
 git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain || return
-git checkout 1.2.0beta
+git checkout 1.2.3beta
 make install
 
 sudo systemctl restart seid
