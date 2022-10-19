dd0e0c327a7d465bb8f454504588a9d8ae76374a
vbloher <vbloher7@gmail.com>
2022-10-19 21:03:47 +0700

    add haqq network

---
+++ haqq/haqq_54211-3/install.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/common.sh)
+
+printLogo
+
+read -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="haqq_54211-3"
+CHAIN_DENOM="aISLM"
+BINARY="haqqd"
+CHEAT_SHEET="https://nodejumper.io/haqq/cheat-sheet"
+
+printLine
+echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+cd || return
+rm -rf haqq
+git clone https://github.com/haqq-network/haqq.git
+cd haqq || return
+git checkout v1.2.0
+make install
+haqqd version # 1.2.0
+
+haqqd config keyring-backend test
+haqqd config chain-id $CHAIN_ID
+haqqd init $NODE_MONIKER --chain-id $CHAIN_ID
+
+curl -L https://github.com/haqq-network/validators-contest/raw/master/genesis.json > $HOME/.haqqd/config/genesis.json
+sha256sum $HOME/.haqqd/config/genesis.json # b93f2650bdf560cab2cf7706ecee72bfba6d947fa57f8b1b8cb887f8b428233f
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.001aISLM"|g' $HOME/.haqqd/config/app.toml
+seeds="62bf004201a90ce00df6f69390378c3d90f6dd7e@seed2.testedge2.haqq.network:26656,23a1176c9911eac442d6d1bf15f92eeabb3981d5@seed1.testedge2.haqq.network:26656"
+peers="eba8c88bb316eb42deed201c84013d2a945537f4@haqq-testnet.nodejumper.io:26656,89d067dc2a046f7b7c1c787740fff18962bf199f@haqq-testnet.paranorm.pro:29656,b3ce1618585a9012c42e9a78bf4a5c1b4bad1123@65.21.170.3:33656,952b9d918037bc8f6d52756c111d0a30a456b3fe@213.239.217.52:29656,85301989752fe0ca934854aecc6379c1ccddf937@65.109.49.111:26556,d648d598c34e0e58ec759aa399fe4534021e8401@109.205.180.81:29956,f2c77f2169b753f93078de2b6b86bfa1ec4a6282@141.95.124.150:20116,eaa6d38517bbc32bdc487e894b6be9477fb9298f@78.107.234.44:45656,37513faac5f48bd043a1be122096c1ea1c973854@65.108.52.192:36656,d2764c55607aa9e8d4cee6e763d3d14e73b83168@66.94.119.47:26656,fc4311f0109d5aed5fcb8656fb6eab29c15d1cf6@65.109.53.53:26656,297bf784ea674e05d36af48e3a951de966f9aa40@65.109.34.133:36656,bc8c24e9d231faf55d4c6c8992a8b187cdd5c214@65.109.17.86:32656"
+sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.haqqd/config/config.toml
+
+# in case of pruning
+sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/.haqqd/config/app.toml
+sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.haqqd/config/app.toml
+sed -i 's|pruning-interval = "0"|pruning-interval = "10"|g' $HOME/.haqqd/config/app.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/haqqd.service >/dev/null <<EOF
+[Unit]
+Description=Haqq Network Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which haqqd) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+haqqd tendermint unsafe-reset-all --home $HOME/.haqqd --keep-addr-book
+
+SNAP_RPC="https://haqq-testnet.nodejumper.io:443"
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height)
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000))
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.haqqd/config/config.toml
+
+sudo systemctl daemon-reload
+sudo systemctl enable haqqd
+sudo systemctl restart haqqd
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
