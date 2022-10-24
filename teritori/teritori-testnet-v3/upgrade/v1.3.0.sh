e4f3c04df12362888039ff0c1e1e61d19cdcea6c
Hdmiimdh <uladzislau.kuzmin@gmail.com>
2022-10-24 12:51:11 +0400

    Add teritori upgrade scripts

---
+++ teritori/teritori-testnet-v3/upgrade/v1.3.0.sh
@@ -0,0 +1,11 @@
+sudo systemctl stop teritorid
+
+cd || return
+rm -rf teritori-chain
+git clone https://github.com/TERITORI/teritori-chain
+cd teritori-chain || return
+git checkout v1.3.0
+make install
+teritorid version # v1.3.0
+
+sudo systemctl restart teritorid
