a0ac3ee22adfd0f756da80a465a093db3bcea78c
vbloher <vbloher7@gmail.com>
2022-10-25 12:14:47 +0700

    add paloma upgrade

---
+++ paloma/paloma-testnet-12/upgrade/v0.11.1.sh
@@ -0,0 +1,19 @@
+sudo systemctl stop palomad
+sudo systemctl stop pigeond
+
+# upgrade paloma
+curl -L https://github.com/palomachain/paloma/releases/download/v0.11.1/paloma_Linux_x86_64.tar.gz > paloma.tar.gz
+tar -xvzf paloma.tar.gz
+rm -rf paloma.tar.gz
+sudo mv -f palomad /usr/local/bin/palomad
+palomad version # v0.11.1
+
+# upgrade pigeon
+curl -L https://github.com/palomachain/pigeon/releases/download/v0.11.0/pigeon_Linux_x86_64.tar.gz > pigeon.tar.gz
+tar -xvzf pigeon.tar.gz
+rm -rf pigeon.tar.gz
+sudo mv -f pigeon /usr/local/bin/pigeon
+pigeon version # v0.11.0
+
+sudo systemctl start palomad
+sudo systemctl start pigeond
