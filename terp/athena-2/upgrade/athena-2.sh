a63cb8b9a2ca6827ddc455e1a130c4c00cd83308
Hdmiimdh <uladzislau.kuzmin@gmail.com>
2022-10-28 16:39:19 +0400

    Add hdmiimdh peer

--- terp/athena-2/upgrade/athena-2.sh
+++ terp/athena-2/upgrade/athena-2.sh
@@ -17,7 +17,7 @@ curl -s https://raw.githubusercontent.com/terpnetwork/test-net/master/athena-2/g
 sha256sum $HOME/.terp/config/genesis.json # b2acc7ba63b05f5653578b05fc5322920635b35a19691dbafd41ef6374b1bc9a
 
 seeds=""
-peers="15f5bc75be9746fd1f712ca046502cae8a0f6ce7@terp-testnet.nodejumper.io:26656,7e5c0b9384a1b9636f1c670d5dc91ba4721ab1ca@23.88.53.28:36656"
+peers="15f5bc75be9746fd1f712ca046502cae8a0f6ce7@terp-testnet.nodejumper.io:26656,7e5c0b9384a1b9636f1c670d5dc91ba4721ab1ca@23.88.53.28:36656,14ca69edabb36c51504f1a760292f8e6b9190bd7@65.21.138.123:28656"
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.terp/config/config.toml
 
 mv $HOME/.terp/priv_validator_state.json.backup $HOME/.terp/data/priv_validator_state.json
