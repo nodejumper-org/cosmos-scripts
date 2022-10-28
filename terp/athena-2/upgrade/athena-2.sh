e5dc11368cac7fbab7a34bc1f600ae86fc3ec76d
Hdmiimdh <uladzislau.kuzmin@gmail.com>
2022-10-28 16:49:20 +0400

    Remove priv_validator_state backup

--- terp/athena-2/upgrade/athena-2.sh
+++ terp/athena-2/upgrade/athena-2.sh
@@ -9,8 +9,6 @@ make install
 terpd version # v0.1.2
 
 terpd config chain-id athena-2
-
-cp $HOME/.terp/data/priv_validator_state.json $HOME/.terp/priv_validator_state.json.backup
 terpd tendermint unsafe-reset-all --home $HOME/.terp
 
 curl -s https://raw.githubusercontent.com/terpnetwork/test-net/master/athena-2/genesis.json > $HOME/.terp/config/genesis.json
@@ -19,7 +17,5 @@ sha256sum $HOME/.terp/config/genesis.json # b2acc7ba63b05f5653578b05fc5322920635
 seeds=""
 peers="15f5bc75be9746fd1f712ca046502cae8a0f6ce7@terp-testnet.nodejumper.io:26656,7e5c0b9384a1b9636f1c670d5dc91ba4721ab1ca@23.88.53.28:36656,14ca69edabb36c51504f1a760292f8e6b9190bd7@65.21.138.123:28656,c989593c89b511318aa6a0c0d361a7a7f4271f28@65.108.124.172:26656"
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.terp/config/config.toml
-
-mv $HOME/.terp/priv_validator_state.json.backup $HOME/.terp/data/priv_validator_state.json
 
 sudo systemctl restart terpd
