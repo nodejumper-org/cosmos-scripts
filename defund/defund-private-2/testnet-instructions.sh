c082aa41d918397a012089e3e77e4d5f2a5c36d0
Hdmiimdh <uladzislau.kuzmin@gmail.com>
2022-11-07 13:42:11 +0400

    Add defund installation scripts

---
+++ defund/defund-private-2/testnet-instructions.sh
@@ -0,0 +1,42 @@
+# Create wallet
+defundd keys add wallet
+
+## Console output
+#- name: wallet
+#  type: local
+#  address: nibi1r9kmadqs9nsppn4wz5yp4rw8zn9545rc4zwvs7
+#  pubkey: '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"Auq9WzVEs5pCoZgr2WctjI7fU+lJCH0I3r6GC1oa0tc0"}'
+#  mnemonic: ""
+
+#!!! SAVE SEED PHRASE
+kite upset hip dirt pet winter thunder slice parent flag sand express suffer chest custom pencil mother bargain remember patient other curve cancel sweet
+
+# Wait util the node is synced, should return FALSE
+defundd status 2>&1 | jq .SyncInfo.catching_up
+
+# Go to https://discord.com/invite/zjkzZwrez5 and request tokens in faucet channel
+
+# Verify the balance
+defundd q bank balances $(defundd keys show wallet -a)
+
+## Console output
+#  balances:
+#  - amount: "10000000"
+#    denom: unibi
+
+# Create validator
+defundd tx staking create-validator \
+--amount=5000000ufetf \
+--pubkey=$(defundd tendermint show-validator) \
+--moniker=<YOUR_VALIDATOR_MONIKER> \
+--chain-id=defund-private-2 \
+--commission-rate=0.1 \
+--commission-max-rate=0.2 \
+--commission-max-change-rate=0.05 \
+--min-self-delegation=1 \
+--fees=2000ufetf \
+--from=wallet \
+-y
+
+# Make sure you see the validator details
+defundd q staking validator $(defundd keys show wallet --bech val -a)
