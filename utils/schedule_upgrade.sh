811a0709e8847f8fdc7bbb3fe370db8ecb066491
vbloher <vbloher7@gmail.com>
2022-11-10 13:40:04 +0700

    add colors to schedule upgrade script

--- utils/schedule_upgrade.sh
+++ utils/schedule_upgrade.sh
@@ -16,7 +16,8 @@ done
 
 printLogo
 
-printCyan "Your $CHAIN_NAME node will be upgraded to version: $VERSION on block height: $TARGET_BLOCK" && sleep 1
+echo -e "Your ${CYAN}$CHAIN_NAME${NC} node will be upgraded to version: ${CYAN}$VERSION${NC} on block height: ${CYAN}$TARGET_BLOCK${NC}" && sleep 1
+echo ""
 
 for (( ; ; )); do
   height=$($BINARY status 2>&1 | jq -r .SyncInfo.latest_block_height)
@@ -26,12 +27,12 @@ for (( ; ; )); do
     $BINARY version --long | head
     break
   else
-    echo "Current block height: $height"
+    echo -e "Current block height: ${CYAN}$height${NC}"
   fi
   sleep 5
 done
 
 printLine
-printCyan "Check logs:            sudo journalctl -u $BINARY -f --no-hostname -o cat"
-printCyan "Check synchronization: $BINARY status 2>&1 | jq .SyncInfo.catching_up"
-printCyan "More commands:         $CHEAT_SHEET"
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
