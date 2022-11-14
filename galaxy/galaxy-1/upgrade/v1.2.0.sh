a3eba39963bf4cddbb5fa4f31603776f10cc97c6
vbloher <vbloher7@gmail.com>
2022-11-14 16:08:56 +0700

    update galaxy script

--- galaxy/galaxy-1/upgrade/v1.2.0.sh
+++ galaxy/galaxy-1/upgrade/v1.2.0.sh
@@ -8,4 +8,10 @@ git checkout v1.2.0
 make install
 galaxyd version # v1.2.0
 
-sudo systemctl restart galaxyd
+cp $HOME/.galaxy/data/priv_validator_state.json $HOME/.galaxy/priv_validator_state.json.backup
+
+cd $HOME/.galaxy || return
+rm -rf data
+wget -O snapshot.tar.gz http://95.216.72.28/data.tar.gz
+tar -xf snapshot.tar.gz -C .
+rm -v snapshot.tar.gz
