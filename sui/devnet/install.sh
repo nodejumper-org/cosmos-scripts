bc81a65f117153d3ff72be9046a7fab11f23a78c
Hdmiimdh <uladzislau.kuzmin@gmail.com>
2022-11-22 17:04:23 +0300

    Add sui version to the comments

--- sui/devnet/install.sh
+++ sui/devnet/install.sh
@@ -15,7 +15,8 @@ version=$(wget -qO- https://api.github.com/repos/nodejumper-org/sui/releases/lat
 curl -L https://github.com/nodejumper-org/sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz > sui-linux-amd64-latest.tar.gz
 sudo tar -xvzf sui-linux-amd64-latest.tar.gz -C /usr/local/bin/
 rm -rf sui-linux-amd64-latest.tar.gz
-sui-node -V && sui -V
+sui-node -V # sui-node 0.16.0
+sui -V # sui 0.16.0
 
 mkdir $HOME/.sui
 curl -L https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml > $HOME/.sui/fullnode.yaml
