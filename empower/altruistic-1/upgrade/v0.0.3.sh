37f560e4a60569a68208ab68a13cf0b579b60a7f
Hdmiimdh <uladzislau.kuzmin@gmail.com>
2022-11-23 13:15:37 +0300

    Add empower upgrade scripts

---
+++ empower/altruistic-1/upgrade/v0.0.3.sh
@@ -0,0 +1,11 @@
+sudo systemctl stop empowerd
+
+cd || return
+rm -rf empowerchain
+git clone https://github.com/empowerchain/empowerchain.git
+cd empowerchain || return
+git checkout v0.0.3
+cd chain || return
+make install
+
+sudo systemctl restart empowerd
