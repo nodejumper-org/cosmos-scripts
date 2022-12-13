160461d195688ed3faa38f2ea033b2488f005f46
Uladzislau K <uladzislau.kuzmin@gmail.com>
2022-12-13 18:26:11 +0300

    Update OKP4 to nemeton-1 testnet

---
+++ okp4/okp4-nemeton-1/install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="okp4-nemeton-1"
+CHAIN_DENOM="uknow"
+BINARY="okp4d"
+CHEAT_SHEET="https://nodejumper.io/okp4-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+cd || return
+rm -rf okp4d
+git clone https://github.com/okp4/okp4d.git
+cd okp4d || return
+git checkout v3.0.0
+make install
+okp4d version # 3.0.0
+
+okp4d config keyring-backend test
+okp4d config chain-id $CHAIN_ID
+okp4d init $NODE_MONIKER --chain-id $CHAIN_ID
+
+curl https://raw.githubusercontent.com/okp4/networks/main/chains/nemeton-1/genesis.json > $HOME/.okp4d/config/genesis.json
+sha256sum $HOME/.okp4d/config/genesis.json #2ec25f81cc2abecbc0da3de45b052ea3314d0d658b1b7f4c7b6a48d09254c742
+
+curl -s https://snapshots2-testnet.nodejumper.io/okp4-testnet/addrbook.json > $HOME/.okp4d/config/addrbook.json
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001uknow"|g' $HOME/.okp4d/config/app.toml
+seeds=""
+peers="256e77194b694d5e2acccc6f1982df62b157bceb@65.108.200.248:26656,fa04503a35476204861f06b75be4839562205527@65.109.85.226:26656,22e7f2d08ec6198ab789d2027a11d5420f5b3a77@192.168.1.16:26656"
+sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.okp4d/config/config.toml
+
+# in case of pruning
+sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/.okp4d/config/app.toml
+sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.okp4d/config/app.toml
+sed -i 's|pruning-interval = "0"|pruning-interval = "17"|g' $HOME/.okp4d/config/app.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/okp4d.service > /dev/null << EOF
+[Unit]
+Description=OKP4 Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which okp4d) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+okp4d tendermint unsafe-reset-all --home $HOME/.okp4d --keep-addr-book
+
+cd "$HOME/.okp4d" || return
+rm -rf data
+rm -rf wasm
+
+SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/okp4-testnet/ | egrep -o ">okp4-nemeton-1.*\.tar.lz4" | tr -d ">")
+curl https://snapshots2-testnet.nodejumper.io/okp4-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.okp4d
+
+sudo systemctl daemon-reload
+sudo systemctl enable okp4d
+sudo systemctl restart okp4d
+sudo journalctl -u okp4d -f --no-hostname -o cat
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
---
+++ okp4/okp4-nemeton-1/testnet-instructions.sh
@@ -0,0 +1,42 @@
+# Create wallet
+okp4d keys add wallet
+
+## Console output
+#- name: wallet
+#  type: local
+#  address: okp410ec4zues3x4xaz89dhv5h4fkdz4cukylxzxt8y
+#  pubkey: '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"Auq9WzVEs5pCoZgr2WctjI7fU+lJCH0I3r6GC1oa0tc0"}'
+#  mnemonic: ""
+
+#!!! SAVE SEED PHRASE
+kite upset hip dirt pet winter thunder slice parent flag sand express suffer chest custom pencil mother bargain remember patient other curve cancel sweet
+
+# Wait util the node is synced, should return FALSE
+okp4d status 2>&1 | jq .SyncInfo.catching_up
+
+# Go to discord and ask for tokens
+
+# Verify the balance
+okp4d q bank balances $(okp4d keys show wallet -a)
+
+## Console output
+#  balances:
+#  - amount: "1000000"
+#    denom: uknow
+
+# Create validator
+okp4d tx staking create-validator \
+--amount=900000uknow \
+--pubkey=$(okp4d tendermint show-validator) \
+--moniker="YOUR_VALIDATOR_MONIKER" \
+--chain-id=okp4-nemeton \
+--commission-rate=0.1 \
+--commission-max-rate=0.2 \
+--commission-max-change-rate=0.05 \
+--min-self-delegation=1 \
+--fees=2000uknow \
+--from=wallet \
+-y
+
+# Make sure you see the validator details
+okp4d q staking validator $(okp4d keys show wallet --bech val -a)
