e956dfa21ac20989537b04aa21535eb8c4d53045
Uladzislau K <uladzislau.kuzmin@gmail.com>
2022-12-16 16:35:13 +0300

    Add Defund upgrade scripts

--- defund/defund-private-3/install.sh
+++ defund/defund-private-3/install.sh
@@ -26,9 +26,9 @@ cd || return
 rm -rf defund
 git clone https://github.com/defund-labs/defund.git
 cd defund || return
-git checkout v0.1.0
+git checkout v0.2.0
 make install
-defundd version # 0.1.0
+defundd version # 0.2.0
 
 defundd config keyring-backend test
 defundd config chain-id $CHAIN_ID
