f9581b9b5367aa53946ae32cb3f0c71dfdadf072
vbloher <vbloher7@gmail.com>
2022-12-17 19:54:36 +0700

    update celestia instructions

--- celestia/mocha/testnet-instructions.sh
+++ celestia/mocha/testnet-instructions.sh
@@ -1,4 +1,4 @@
-# Create wallet
+# Create VALIDATOR wallet
 celestia-appd keys add wallet
 
 ## Console output
@@ -11,13 +11,28 @@ celestia-appd keys add wallet
 #!!! SAVE SEED PHRASE
 kite upset hip dirt pet winter thunder slice parent flag sand express suffer chest custom pencil mother bargain remember patient other curve cancel sweet
 
-# Wait util the node is synced, should return FALSE
-celestia-appd status 2>&1 | jq .SyncInfo.catching_up
+# Create ORCHESTRATOR wallet
+celestia-appd keys add orchestrator
 
-# Go to https://discord.gg/kUSueaB22b and request tokens in faucet channel
+## Console output
+#  name: orchestrator
+#  type: local
+#- address: celestia1x5c4vj5u0wcgvdclrr6mk2ekrs7emcjpkfa3dw
+#  pubkey: '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"Aq4dSZa+5A3SR3+dZVWkXUcHmWuZQ+Xx7Iu1KHHtnWCW"}'
+
+#!!! SAVE SEED PHRASE
+infant wasp injury parrot morning bag wet clean address pact hobby emerge raccoon rain degree dwarf gas defense deposit maximum order cross powder monitor
+
+# Create ETH address in metamask
+
+# Wait util the node is synced, should return FALSE
+curl -s localhost:26657/status | jq .result.sync_info.catching_up
+
+# Go to https://discord.gg/kUSueaB22b and request tokens in faucet channel for validator and orchestrator addresses
 
 # Verify the balance
 celestia-appd q bank balances $(celestia-appd keys show wallet -a)
+celestia-appd q bank balances $(celestia-appd keys show orchestrator -a)
 
 ## Console output
 #  balances:
@@ -34,8 +49,12 @@ celestia-appd tx staking create-validator \
 --commission-max-rate=0.2 \
 --commission-max-change-rate=0.05 \
 --min-self-delegation=1 \
---fees=2000utia \
 --from=wallet \
+--evm-address="YOUR_ETH_ADDRESS" \
+--orchestrator-address="YOUR_ORCHESTRATOR_ADDRESS" \
+--gas=auto \
+--gas-adjustment=1.5 \
+--fees=1500utia \
 -y
 
 # Make sure you see the validator details
