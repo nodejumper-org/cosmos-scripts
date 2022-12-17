33bc7e9fe5496bbeda708eb461f580fd7ad7faf7
vbloher <vbloher7@gmail.com>
2022-12-17 23:55:47 +0700

    update terp upgrade script

--- terp/athena-2/upgrade/v0.2.0.sh
+++ terp/athena-2/upgrade/v0.2.0.sh
@@ -1,5 +1,10 @@
 sudo systemctl stop terpd
 
+# backup chain data
+cd || return
+cp .terp .terp_backup
+
+# build new binary
 cd || return
 rm -rf terp-core
 git clone https://github.com/terpnetwork/terp-core.git
@@ -8,4 +13,23 @@ git checkout v0.2.0
 make install
 terpd version # v0.2.0
 
-sudo systemctl restart terpd
+# update genesis
+curl -#  https://raw.githubusercontent.com/terpnetwork/test-net/master/athena-2/0.2.0/genesis.json > ~/.terp/config/genesis.json
+
+# add skip upgrades flag to service file
+sudo tee /etc/systemd/system/terpd.service > /dev/null << EOF
+[Unit]
+Description=TerpNetwork Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which terpd) start --unsafe-skip-upgrades 1497396
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl start terpd
