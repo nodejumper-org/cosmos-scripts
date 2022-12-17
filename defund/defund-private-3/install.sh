ec5cd933d8de8a8f3786f75ea567cd56bc5487a9
Uladzislau K <uladzislau.kuzmin@gmail.com>
2022-12-17 13:22:54 +0300

    Update kj nodes explorer link

--- defund/defund-private-3/install.sh
+++ defund/defund-private-3/install.sh
@@ -26,9 +26,9 @@ cd || return
 rm -rf defund
 git clone https://github.com/defund-labs/defund.git
 cd defund || return
-git checkout v0.2.0
+git checkout v0.2.1
 make install
-defundd version # 0.2.0
+defundd version # 0.2.1
 
 defundd config keyring-backend test
 defundd config chain-id $CHAIN_ID
