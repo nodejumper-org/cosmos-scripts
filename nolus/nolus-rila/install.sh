f01384b434fda81132e5815de1a7ba973c664e8b
Uladzislau K <uladzislau.kuzmin@gmail.com>
2022-12-20 14:01:36 +0300

    Add Nolus

---
+++ nolus/nolus-rila/install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="nolus-rila"
+CHAIN_DENOM="unls"
+BINARY="nolusd"
+CHEAT_SHEET="https://nodejumper.io/nolus-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+cd || return
+rm -rf nolus-core
+git clone https://github.com/Nolus-Protocol/nolus-core.git
+cd nolus-core || return
+git checkout v0.1.39
+make install
+nolusd version # 0.1.39
+
+nolusd config keyring-backend test
+nolusd config chain-id $CHAIN_ID
+nolusd init $NODE_MONIKER --chain-id $CHAIN_ID
+
+curl https://raw.githubusercontent.com/Nolus-Protocol/nolus-networks/main/testnet/nolus-rila/genesis.json > $HOME/.nolus/config/genesis.json
+sha256sum $HOME/.nolus/config/genesis.json #d22ea6488afe58478c54afeb2d6b5a45622c797dfd75c91a8653eb1f094173c5
+
+curl -s https://snapshots4-testnet.nodejumper.io/nolus-testnet/addrbook.json > $HOME/.nolus/config/addrbook.json
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001unls"|g' $HOME/.nolus/config/app.toml
+seeds=""
+peers=""
+sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.nolus/config/config.toml
+
+# in case of pruning
+sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/.nolus/config/app.toml
+sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.nolus/config/app.toml
+sed -i 's|pruning-interval = "0"|pruning-interval = "17"|g' $HOME/.nolus/config/app.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/nolusd.service > /dev/null << EOF
+[Unit]
+Description=Nolus Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which nolusd) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+nolusd tendermint unsafe-reset-all --home $HOME/.nolus --keep-addr-book
+
+cd "$HOME/.nolus" || return
+rm -rf data
+rm -rf wasm
+
+SNAP_NAME=$(curl -s https://snapshots4-testnet.nodejumper.io/nolus-testnet/ | egrep -o ">nolus-rila.*\.tar.lz4" | tr -d ">")
+curl https://snapshots4-testnet.nodejumper.io/nolus-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.nolus
+
+sudo systemctl daemon-reload
+sudo systemctl enable nolusd
+sudo systemctl restart nolusd
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
---
+++ nolus/nolus-rila/testnet-instructions.sh
@@ -0,0 +1,46 @@
+# Create wallet
+nolusd keys add wallet
+
+## Console output
+#- name: wallet
+#  type: local
+#  address: nolus1y7lfy9909gjfrrudwa3y8z0ndp9xlf0anvrkfw
+#  pubkey: '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"Auq9WzVEs5pCoZgr2WctjI7fU+lJCH0I3r6GC1oa0tc0"}'
+#  mnemonic: ""
+
+#!!! SAVE SEED PHRASE
+kite upset hip dirt pet winter thunder slice parent flag sand express suffer chest custom pencil mother bargain remember patient other curve cancel sweet
+
+#!!! SAVE PRIVATE VALIDATOR KEY
+cat $HOME/.nolus/config/priv_validator_key.json
+
+# Wait util the node is synced, should return FALSE
+nolusd status 2>&1 | jq .SyncInfo.catching_up
+
+# Go to discord channel #testnet-faucet and paste
+$request YOUR_WALLET_ADDRESS
+
+# Verify the balance
+nolusd q bank balances $(nolusd keys show wallet -a)
+
+## Console output
+#  balances:
+#  - amount: "2000000"
+#    denom: unls
+
+# Create validator
+nolusd tx staking create-validator \
+--amount=1500000unls \
+--pubkey=$(nolusd tendermint show-validator) \
+--moniker="YOUR_VALIDATOR_MONIKER" \
+--chain-id=nolus-rila \
+--commission-rate=0.1 \
+--commission-max-rate=0.2 \
+--commission-max-change-rate=0.05 \
+--min-self-delegation=1 \
+--fees=20000unls \
+--from=wallet \
+-y
+
+# Make sure you see the validator details
+nolusd q staking validator $(nolusd keys show wallet --bech val -a)
