78a23e30971d39a2e30cc617d5e9a2b298a8c657
vbloher <vbloher7@gmail.com>
2022-12-28 15:16:33 +0700

    add pylonsd 1.1.2

---
+++ pylons/pylons-mainnet-1/upgrade/v1.1.2.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop pylonsd
+
+cd || return
+rm -rf pylons
+git clone https://github.com/Pylons-tech/pylons
+cd pylons || return
+git checkout v1.1.2
+make install
+
+sudo systemctl restart pylonsd
