ca14bbbeca5363b47f778f82ec8b98ad1d9cd595
vbloher <vbloher7@gmail.com>
2023-01-04 13:04:26 +0700

    add evmos upgrade

---
+++ evmos/evmos_9001-2/upgrade/v10.0.0.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop evmosd
+
+cd || return
+rm -rf evmos
+gir clone https://github.com/evmos/evmos.git
+cd evmos || return
+git checkout v10.0.0
+make install
+
+sudo systemctl start evmosd
