407fc3af51742bf102e9b5a84b73cfbc388aa96c
vbloher <vbloher7@gmail.com>
2023-01-29 14:16:47 +0700

    fix sed

--- cardchain/testnet3/install.sh
+++ cardchain/testnet3/install.sh
@@ -49,7 +49,7 @@ sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.Cardc
 sed -i 's|^pruning-interval *=.*|pruning-interval = "'$PRUNING_INTERVAL'"|g' $HOME/.Cardchain/config/app.toml
 sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' $HOME/.Cardchain/config/app.toml
 
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001ubpf"|g' $HOME/.bcna/config/app.toml
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001ubpf"|g' $HOME/.Cardchain/config/app.toml
 sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.Cardchain/config/config.toml
 
 printCyan "5. Starting service and synchronization..." && sleep 1
