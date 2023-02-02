d8b22cb52aef7f99313c584e9e844db0852f7b68
vbloher <vbloher7@gmail.com>
2023-02-02 16:14:24 +0700

    remove uptick binary dir after install

--- uptick/uptick_7000-2/install.sh
+++ uptick/uptick_7000-2/install.sh
@@ -27,8 +27,8 @@ printCyan "4. Building binaries..." && sleep 1
 curl -L -k https://github.com/UptickNetwork/uptick/releases/download/v0.2.4/uptick-linux-amd64-v0.2.4.tar.gz > uptick.tar.gz
 tar -xvzf uptick.tar.gz
 sudo mv -f uptick-linux-amd64-v0.2.4/uptickd /usr/local/bin/uptickd
-rm -rf uptick.tar.gz
-rm -rf uptick-v0.2.4
+rm uptick.tar.gz
+rm -rf uptick-linux-amd64-v0.2.4
 uptickd version # v0.2.4
 
 uptickd config keyring-backend test
