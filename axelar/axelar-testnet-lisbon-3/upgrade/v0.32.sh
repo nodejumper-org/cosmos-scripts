98908fd2382abadcb8252d4185228f38fa1fc223
vbloher <vbloher7@gmail.com>
2023-02-21 16:43:38 +0700

    add axelar 0.32 upgrade scripts

---
+++ axelar/axelar-testnet-lisbon-3/upgrade/v0.32.sh
@@ -0,0 +1,15 @@
+sudo systemctl stop axelard
+sudo systemctl stop vald
+sudo systemctl stop tofnd
+
+cd || return
+rm -rf axelar-core
+git clone https://github.com/axelarnetwork/axelar-core.git
+cd axelar-core || return
+git checkout v0.32
+make build
+cp bin/axelard "$HOME/.axelar_testnet/bin/axelard"
+
+sudo systemctl start axelard
+sudo systemctl start vald
+sudo systemctl start tofnd
