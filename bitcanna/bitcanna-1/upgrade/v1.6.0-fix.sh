738490cf7d9ee65872c8af1402a5796453974a4e
vbloher <vbloher7@gmail.com>
2023-03-02 22:33:29 +0700

    add fix upgrade

--- bitcanna/bitcanna-1/upgrade/v1.6.0-fix.sh
+++ bitcanna/bitcanna-1/upgrade/v1.6.0-fix.sh
@@ -4,7 +4,7 @@ cd || return
 rm -rf bcna
 git clone https://github.com/BitCannaGlobal/bcna.git
 cd bcna || return
-git checkout v1.6.0
+git checkout v1.6.0-fix
 make install
 
 sudo systemctl start bcnad
