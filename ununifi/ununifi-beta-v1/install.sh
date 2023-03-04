9dbfe26309d13867d2df5bb610740f293c159914
vbloher <vbloher7@gmail.com>
2023-03-04 13:24:45 +0700

    change snapshot server

--- ununifi/ununifi-beta-v1/install.sh
+++ ununifi/ununifi-beta-v1/install.sh
@@ -36,7 +36,7 @@ ununifid config chain-id $CHAIN_ID
 ununifid init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
 curl -s https://raw.githubusercontent.com/UnUniFi/network/main/launch/ununifi-beta-v1/genesis.json > $HOME/.ununifi/config/genesis.json
-# TODO curl -s https://snapshots.nodejumper.io/ununifi/addrbook.json > $HOME/.ununifi/config/addrbook.json
+# TODO curl -s https://snapshots1.nodejumper.io/ununifi/addrbook.json > $HOME/.ununifi/config/addrbook.json
 
 SEEDS="fa38d2a851de43d34d9602956cd907eb3942ae89@a.ununifi.cauchye.net:26656,404ea79bd31b1734caacced7a057d78ae5b60348@b.ununifi.cauchye.net:26656,1357ac5cd92b215b05253b25d78cf485dd899d55@[2600:1f1c:534:8f02:7bf:6b31:3702:2265]:26656,25006d6b85daeac2234bcb94dafaa73861b43ee3@[2600:1f1c:534:8f02:a407:b1c6:e8f5:94b]:26656,caf792ed396dd7e737574a030ae8eabe19ecdf5c@[2600:1f1c:534:8f02:b0a4:dbf6:e50b:d64e]:26656,796c62bb2af411c140cf24ddc409dff76d9d61cf@[2600:1f1c:534:8f02:ca0e:14e9:8e60:989e]:26656,cea8d05b6e01188cf6481c55b7d1bc2f31de0eed@[2600:1f1c:534:8f02:ba43:1f69:e23a:df6b]:26656"
 PEERS=""
@@ -69,8 +69,8 @@ EOF
 ununifid tendermint unsafe-reset-all --home $HOME/.ununifi --keep-addr-book
 
 # TODO:
-#SNAP_NAME=$(curl -s https://snapshots-testnet.nodejumper.io/ununifi/info.json | jq -r .fileName)
-#curl "https://snapshots.nodejumper.io/ununifi/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C $HOME/.ununifi
+#SNAP_NAME=$(curl -s https://snapshots1.nodejumper.io/ununifi/info.json | jq -r .fileName)
+#curl "https://snapshots1.nodejumper.io/ununifi/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C $HOME/.ununifi
 
 sudo systemctl daemon-reload
 sudo systemctl enable ununifid
