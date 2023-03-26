f7a25c7f6203454cc42c3dbe477085fc018f85de
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-03-26 15:19:02 +0700

    Add Uptick intallation scripts

---
+++ uptick/uptick_117-1/install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="uptick_117-1"
+CHAIN_DENOM="auptick"
+BINARY_NAME="uptickd"
+BINARY_VERSION_TAG="v0.2.4"
+CHEAT_SHEET="https://nodejumper.io/uptick/cheat-sheet"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+cd $HOME || return
+rm -rf uptick
+git clone https://github.com/UptickNetwork/uptick.git
+cd uptick || return
+git checkout v0.2.4
+make install
+uptickd version # HEAD-3d1e40d0d42b420ac02c624a2d6e8225c0b5991b
+
+uptickd config chain-id $CHAIN_ID
+uptickd init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -s https://raw.githubusercontent.com/UptickNetwork/uptick-mainnet/master/uptick_117-1/genesis.json > $HOME/.uptickd/config/genesis.json
+curl -s https://snapshots2.nodejumper.io/uptick/addrbook.json > $HOME/.uptickd/config/addrbook.json
+
+SEEDS="f97a75fb69d3a5fe893dca7c8d238ccc0bd66a8f@uptick.seed.brocha.in:30600"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.uptickd/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.uptickd/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.uptickd/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.uptickd/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.uptickd/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.001auptick"|g' $HOME/.uptickd/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.uptickd/config/config.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/uptickd.service > /dev/null << EOF
+[Unit]
+Description=Uptick Network Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which uptickd) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+uptickd tendermint unsafe-reset-all --home $HOME/.uptickd/ --keep-addr-book
+
+SNAP_NAME=$(curl -s https://snapshots2.nodejumper.io/uptick/info.json | jq -r .fileName)
+curl "https://snapshots2.nodejumper.io/uptick/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.uptickd"
+
+sudo systemctl daemon-reload
+sudo systemctl enable uptickd
+sudo systemctl start uptickd
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
