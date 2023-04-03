c4ac43bfae5f523259907866a5e836890d224315
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-04-03 22:17:28 +0700

    Fix menu for small screens

--- sei/atlantic-1/install.sh
+++ sei/atlantic-1/install.sh
@@ -1,36 +1,82 @@
 #!/bin/bash
 
-curl -s https://get.nibiru.fi/pricefeeder! | bash
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
 
-export CHAIN_ID="nibiru-itn-1"
-export GRPC_ENDPOINT="localhost:9090"
-export WEBSOCKET_ENDPOINT="ws://localhost:26657/websocket"
-export EXCHANGE_SYMBOLS_MAP='{ "bitfinex": { "ubtc:uusd": "tBTCUSD", "ueth:uusd": "tETHUSD", "uusdt:uusd": "tUSTUSD" }, "binance": { "ubtc:uusd": "BTCUSD", "ueth:uusd": "ETHUSD", "uusdt:uusd": "USDTUSD", "uusdc:uusd": "USDCUSD", "uatom:uusd": "ATOMUSD", "ubnb:uusd": "BNBUSD", "uavax:uusd": "AVAXUSD", "usol:uusd": "SOLUSD", "uada:uusd": "ADAUSD", "ubtc:unusd": "BTCUSD", "ueth:unusd": "ETHUSD", "uusdt:unusd": "USDTUSD", "uusdc:unusd": "USDCUSD", "uatom:unusd": "ATOMUSD", "ubnb:unusd": "BNBUSD", "uavax:unusd": "AVAXUSD", "usol:unusd": "SOLUSD", "uada:unusd": "ADAUSD" } }'
-export FEEDER_MNEMONIC="YOUR_SEED_PHRASE"
+printLogo
 
-sudo tee /etc/systemd/system/pricefeeder.service > /dev/null << EOF
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="atlantic-1"
+CHAIN_DENOM="usei"
+BINARY_NAME="seid"
+BINARY_VERSION_TAG="v1.2.2beta-postfix"
+CHEAT_SHEET="https://nodejumper.io/sei-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+cd || return
+rm -rf sei-chain
+git clone https://github.com/sei-protocol/sei-chain.git
+cd sei-chain || return
+git checkout 1.2.2beta-postfix
+make install
+seid version # 1.2.2beta-postfix
+
+seid config keyring-backend test
+seid config chain-id $CHAIN_ID
+seid init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -s https://raw.githubusercontent.com/sei-protocol/testnet/main/sei-incentivized-testnet/genesis.json > $HOME/.sei/config/genesis.json
+curl -s https://snapshots2-testnet.nodejumper.io/sei-testnet/addrbook.json > $HOME/.sei/config/addrbook.json
+
+SEEDS="df1f6617ff5acdc85d9daa890300a57a9d956e5e@sei-atlantic-1.seed.rhinostake.com:16660"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.sei/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.sei/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.sei/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.sei/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.sei/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001usei"|g' $HOME/.sei/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.sei/config/config.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/seid.service > /dev/null << EOF
 [Unit]
-Description=Nibiru Pricefeeder
+Description=Sei Protocol Node
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=/usr/local/bin/pricefeeder
+ExecStart=$(which seid) start
 Restart=on-failure
 RestartSec=10
-LimitNOFILE=65535
-Environment=CHAIN_ID='$CHAIN_ID'
-Environment=GRPC_ENDPOINT='$GRPC_ENDPOINT'
-Environment=WEBSOCKET_ENDPOINT='$WEBSOCKET_ENDPOINT'
-Environment=EXCHANGE_SYMBOLS_MAP='$EXCHANGE_SYMBOLS_MAP'
-Environment=FEEDER_MNEMONIC='$FEEDER_MNEMONIC'
+LimitNOFILE=10000
 [Install]
 WantedBy=multi-user.target
 EOF
 
-sudo systemctl daemon-reload
-sudo systemctl enable pricefeeder
-sudo systemctl start pricefeeder
-sudo journalctl -u pricefeeder -f --no-hostname -o cat
+seid tendermint unsafe-reset-all --home $HOME/.sei --keep-addr-book
 
-# Check number of TX
-nibid q txs --events='message.sender=YOUR_WALLET_ADDRESS&message.action=/nibiru.oracle.v1beta1.MsgAggregateExchangeRateVote' -oj|jq -r '.total_count'
+SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/sei-testnet/info.json | jq -r .fileName)
+curl "https://snapshots2-testnet.nodejumper.io/sei-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.sei"
+
+sudo systemctl daemon-reload
+sudo systemctl enable seid
+sudo systemctl start seid
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
