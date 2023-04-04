84f373c0fd5c91b43114088310216f2aa5286f65
vbloher <vbloher7@gmail.com>
2023-04-04 11:00:15 +0700

    update paloma upgrade script

--- paloma/paloma-testnet-15/upgrade/v0.11.7.sh
+++ paloma/paloma-testnet-15/upgrade/v0.11.7.sh
@@ -1,10 +1,12 @@
 sudo systemctl stop palomad
 
 cd || return
-curl -L https://github.com/palomachain/paloma/releases/download/v0.11.7/paloma_Linux_x86_64.tar.gz > paloma.tar.gz
-tar -xvzf paloma.tar.gz
-rm -rf paloma.tar.gz
-sudo mv -f palomad /usr/local/bin/palomad
+rm -rf paloma
+git clone https://github.com/palomachain/paloma.git
+cd paloma || return
+git checkout v0.11.7
+make install
+sudo mv -f $HOME/go/bin/palomad /usr/local/bin/palomad
 palomad version # v0.11.7
 
 sudo systemctl start palomad
