722508b4fe1110739de9c5bb9abc9f40c1082a4b
vbloher <vbloher7@gmail.com>
2023-04-11 11:13:09 +0700

    add v0.10.0 for omniflixhub

---
+++ omniflix/omniflixhub-1/upgrade/v0.10.0.sh
@@ -0,0 +1,11 @@
+sudo systemctl stop omniflixhubd
+
+cd || return
+rm -rf omniflixhub
+git clone https://github.com/Omniflix/omniflixhub.git
+cd omniflixhub || return
+git checkout v0.10.0
+make install
+omniflixhubd version # 0.10.0
+
+sudo systemctl start omniflixhubd
