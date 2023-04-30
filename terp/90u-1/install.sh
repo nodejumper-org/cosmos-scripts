2c343ef0b63822a27a8e297da10883c0d0ca8090
vbloher <vbloher7@gmail.com>
2023-04-30 11:20:55 +0700

    add 90u-1 terp testnet network

---
+++ terp/90u-1/install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="90u-1"
+CHAIN_DENOM="uthiol"
+BINARY_NAME="terpd"
+BINARY_VERSION_TAG="v1.0.1"
+CHEAT_SHEET="https://nodejumper.io/terpnetwork-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+cd || return
+rm -rf terp-core
+git clone https://github.com/terpnetwork/terp-core.git
+cd terp-core || return
+git checkout v1.0.1
+make install
+terpd version # 1.0.1
+
+terpd config keyring-backend test
+terpd config chain-id $CHAIN_ID
+terpd init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -s https://raw.githubusercontent.com/terpnetwork/test-net/master/90u-1/genesis.json > $HOME/.terp/config/genesis.json
+curl -s https://snapshots1-testnet.nodejumper.io/terpnetwork-testnet/addrbook.json > $HOME/.terp/config/addrbook.json
+
+SEEDS=""
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.terp/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.terp/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.terp/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.terp/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.terp/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.001uthiol"|g' $HOME/.terp/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.terp/config/config.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/terpd.service > /dev/null << EOF
+[Unit]
+Description=TerpNetwork Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which terpd) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+terpd tendermint unsafe-reset-all --home $HOME/.terp --keep-addr-book
+
+SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/terpnetwork-testnet/info.json | jq -r .fileName)
+curl "https://snapshots1-testnet.nodejumper.io/terpnetwork-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.terp"
+
+sudo systemctl daemon-reload
+sudo systemctl enable terpd
+sudo systemctl start terpd
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
