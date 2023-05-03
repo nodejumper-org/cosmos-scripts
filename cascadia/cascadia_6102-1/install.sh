618b340affe93c057bf83d598bd6e3786a13be36
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-05-03 17:20:18 +0700

    Add Cascadia

---
+++ cascadia/cascadia_6102-1/install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="cascadia_6102-1"
+CHAIN_DENOM="aCC"
+BINARY_NAME="cascadiad"
+BINARY_VERSION_TAG="v0.1.1"
+CHEAT_SHEET="https://nodejumper.io/cascadia-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+curl -L https://github.com/CascadiaFoundation/cascadia/releases/download/v0.1.1/cascadiad-v0.1.1-linux-amd64 -o cascadiad
+chmod +x cascadiad
+sudo mv cascadiad /usr/local/bin
+cascadiad version # ac03925
+
+cascadiad config keyring-backend test
+cascadiad config chain-id $CHAIN_ID
+cascadiad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -L https://github.com/CascadiaFoundation/chain-configuration/raw/master/testnet/genesis.json.gz -o ~/.cascadiad/config/genesis.zip
+gunzip -c ~/.cascadiad/config/genesis.zip > ~/.cascadiad/config/genesis.json
+rm -rf ~/.cascadiad/config/genesis.zip
+
+curl -s https://snapshots1-testnet.nodejumper.io/cascadia-testnet/addrbook.json > $HOME/.cascadiad/config/addrbook.json
+
+SEEDS="42c4a78f39935df1c20b51c4b0d0a21db8f01c88@cascadia-testnet-seed.itrocket.net:40656"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.cascadiad/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.cascadiad/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.cascadiad/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.cascadiad/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.cascadiad/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001aCC"|g' $HOME/.cascadiad/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.cascadiad/config/config.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/cascadiad.service > /dev/null << EOF
+[Unit]
+Description=Cascadia Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which cascadiad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cascadiad tendermint unsafe-reset-all --home $HOME/.cascadiad --keep-addr-book
+
+SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/cascadia-testnet/info.json | jq -r .fileName)
+curl "https://snapshots1-testnet.nodejumper.io/cascadia-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.cascadiad"
+
+sudo systemctl daemon-reload
+sudo systemctl enable cascadiad
+sudo systemctl start cascadiad
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
---
+++ cascadia/cascadia_6102-1/testnet-instructions.sh
@@ -0,0 +1,50 @@
+# create wallet
+cascadiad keys add wallet
+
+## console output:
+#- name: wallet
+#  type: local
+#  address: cascadia104g6jcsg6fmmvn2a2t0nqg4ex68xgqdt2mpqne
+#  pubkey: '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"Auq9WzVEs5pCoZgr2WctjI7fU+lJCH0I3r6GC1oa0tc0"}'
+#  mnemonic: ""
+
+#!!! SAVE SEED PHRASE
+kite upset hip dirt pet winter thunder slice parent flag sand express suffer chest custom pencil mother bargain remember patient other curve cancel sweet
+
+#!!! SAVE PRIVATE VALIDATOR KEY
+cat $HOME/.cascadiad/config/priv_validator_key.json
+
+# wait util the node is synced, should return FALSE
+cascadiad status 2>&1 | jq .SyncInfo.catching_up
+
+# get EVM address of your wallet
+cascadiad address-converter $(cascadiad keys show wallet -a)
+
+# go to https://www.cascadia.foundation/faucet and faucet some tokens to the EVM wallet
+
+# verify the balance
+cascadiad q bank balances $(cascadiad keys show wallet -a)
+
+## console output:
+#  balances:
+#  - amount: "2000000000000000000"
+#    denom: aCC
+
+# create validator
+cascadiad tx staking create-validator \
+--amount=1000000000000000000aCC \
+--pubkey=$(cascadiad tendermint show-validator) \
+--moniker="$NODE_MONIKER" \
+--chain-id=cascadia_6102-1 \
+--commission-rate=0.1 \
+--commission-max-rate=0.2 \
+--commission-max-change-rate=0.05 \
+--min-self-delegation=1 \
+--gas-prices=7aCC \
+--gas-adjustment=1.5 \
+--gas=auto \
+--from=wallet \
+-y
+
+# make sure you see the validator details
+cascadiad q staking validator $(cascadiad keys show wallet --bech val -a)
