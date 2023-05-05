f0bb116e69b38835cea6496ac480598e54b6d536
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-05-05 12:13:08 +0700

    Add sei price feeder installation

--- sei/atlantic-2/testnet-instructions.sh
+++ sei/atlantic-2/testnet-instructions.sh
@@ -44,3 +44,57 @@ seid tx staking create-validator \
 
 # make sure you see the validator details
 seid q staking validator $(seid keys show wallet --bech val -a)
+
+# install pricefeeder
+cd || return
+cd sei-chain || return
+make install-price-feeder
+price-feeder version
+
+mkdir -p $HOME/.price-feeder
+cp $HOME/sei-chain/oracle/price-feeder/config.example.toml $HOME/.price-feeder/price-feeder.toml
+
+seid keys add feeder-wallet --keyring-backend os
+seid tx bank send wallet YOUR_FEEDER_ADDRESS 1000000usei --from wallet --chain-id atlantic-2 --fees 2000usei -y
+seid q bank balances $(seid keys show feeder-wallet --keyring-backend os -a)
+
+CHAIN_ID=atlantic-2
+KEYRING_PASSWORD=YOUR_KEYRING_PASSWORD
+WALLET_ADDRESS=$(seid keys show wallet -a)
+FEEDER_ADDRESS=$(seid keys show feeder-wallet --keyring-backend os -a)
+VALIDATOR_ADDRESS=$(seid keys show wallet --bech val -a)
+GRPC="localhost:9090"
+RPC="http://localhost:26657"
+
+seid tx oracle set-feeder $FEEDER_ADDRESS  --from wallet --fees 2000usei -y
+
+sed -i '/^dir *=.*/a pass = ""' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^address *=.*|address = "'$FEEDER_ADDRESS'"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^chain_id *=.*|chain_id = "'$CHAIN_ID'"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^validator *=.*|validator = "'$VALIDATOR_ADDRESS'"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^backend *=.*|backend = "os"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^dir *=.*|dir = "'$HOME/.sei'"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^pass *=.*|pass = "'$KEYRING_PASSWORD'"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^grpc_endpoint *=.*|grpc_endpoint = "'$GRPC'"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^tmrpc_endpoint *=.*|tmrpc_endpoint = "'$RPC'"|g' $HOME/.price-feeder/price-feeder.toml
+sed -i 's|^global-labels *=.*|global-labels = [["chain_id", "'$CHAIN_ID'"]]|g' $HOME/.price-feeder/price-feeder.toml
+
+sudo tee /etc/systemd/system/price-feeder.service > /dev/null << EOF
+[Unit]
+Description=Sei Price Feeder
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which price-feeder) $HOME/.price-feeder/price-feeder.toml --log-level debug
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+Environment="PRICE_FEEDER_PASS=$KEYRING_PASSWORD"
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable price-feeder
+sudo systemctl start price-feeder
+sudo journalctl -u price-feeder -f --no-hostname -o cat
