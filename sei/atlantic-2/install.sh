a5125835614b622d494323fc6227dca85b7b8bd7
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-06-02 16:20:52 +0700

    Update sei sync scripts

--- sei/atlantic-2/install.sh
+++ sei/atlantic-2/install.sh
@@ -9,7 +9,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="atlantic-2"
 CHAIN_DENOM="usei"
 BINARY_NAME="seid"
-BINARY_VERSION_TAG="2.0.47beta"
+BINARY_VERSION_TAG="3.0.2"
 CHEAT_SHEET="https://nodejumper.io/sei-testnet/cheat-sheet"
 
 printLine
@@ -28,9 +28,9 @@ cd || return
 rm -rf sei-chain
 git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain || return
-git checkout 2.0.47beta
+git checkout 3.0.2
 make install
-seid version # 2.0.47beta
+seid version # 3.0.2
 
 seid config keyring-backend test
 seid config chain-id $CHAIN_ID
