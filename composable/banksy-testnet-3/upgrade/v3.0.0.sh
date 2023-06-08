fa903e9b2abdcc020bf842305360ab7ea4e69ecd
vbloher <vbloher7@gmail.com>
2023-06-08 13:24:41 +0700

    add composable upgrade

---
+++ composable/banksy-testnet-3/upgrade/v3.0.0.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop banksyd
+
+cd || return
+rm -rf composable-testnet
+git clone https://github.com/notional-labs/composable-testnet.git
+cd composable-testnet || return
+git checkout v3.0.0
+make install
+
+sudo systemctl start banksyd
