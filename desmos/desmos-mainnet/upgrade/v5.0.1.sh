032c3da1bd63934b6660970d65750648fb2fc645
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-06-09 13:11:03 +0700

    Update Desmos upgrade script

--- desmos/desmos-mainnet/upgrade/v5.0.1.sh
+++ desmos/desmos-mainnet/upgrade/v5.0.1.sh
@@ -4,8 +4,8 @@ cd || return
 rm -rf desmos
 git clone https://github.com/desmos-labs/desmos.git
 cd desmos || return
-git checkout v5.0.0
+git checkout v5.0.1
 make install
-desmos version # 5.0.0
+desmos version # 5.0.1
 
 sudo systemctl start desmosd
