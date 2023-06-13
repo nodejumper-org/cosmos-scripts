de4595c434b502556ff228807f424399464b04c2
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-06-13 14:51:15 +0700

    Add UnUnify upgrade instructions

---
+++ ununifi/ununifi-beta-v1/upgrade/v3.0.0.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop ununifid
+
+cd || return
+rm -rf ununifi
+git clone https://github.com/UnUniFi/chain ununifi
+cd ununifi || return
+git checkout v3.0.0
+make install
+
+sudo systemctl start ununifid
