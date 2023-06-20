8eb81bf609281a6f59c19e59027dfb73cd4772ea
vbloher <vbloher7@gmail.com>
2023-06-20 09:36:20 +0700

    update composable mainnet binary version

--- composable/centauri-1/install.sh
+++ composable/centauri-1/install.sh
@@ -9,7 +9,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="centauri-1"
 CHAIN_DENOM="ppica"
 BINARY_NAME="banksyd"
-BINARY_VERSION_TAG="v3.1.0"
+BINARY_VERSION_TAG="v3.1.1"
 CHEAT_SHEET="https://nodejumper.io/сomposable/cheat-sheet"
 
 printLine
@@ -28,7 +28,7 @@ cd || return
 rm -rf composable-centauri
 git clone https://github.com/notional-labs/composable-centauri
 cd composable-centauri || return
-git checkout v3.1.0
+git checkout v3.1.1
 make install
 
 banksyd config chain-id $CHAIN_ID
