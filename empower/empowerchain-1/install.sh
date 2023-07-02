f2a4f7ed4b403ed1836aa87c1b7ec29240491d82
vbloher <vbloher7@gmail.com>
2023-07-02 20:31:24 +0700

    add empowerchain mainnet

---
+++ empower/empowerchain-1/install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="empowerchain-1"
+CHAIN_DENOM="umpwr"
+BINARY_NAME="empowerd"
+BINARY_VERSION_TAG="v1.0.0"
+CHEAT_SHEET="https://nodejumper.io/empower-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+cd || return
+rm -rf empowerchain
+git clone https://github.com/EmpowerPlastic/empowerchain
+cd empowerchain/chain || return
+git checkout v1.0.0
+make install
+empowerd version # 1.0.0
+
+empowerd config keyring-backend test
+empowerd config chain-id $CHAIN_ID
+empowerd init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -L https://github.com/EmpowerPlastic/empowerchain/raw/main/mainnet/empowerchain-1/genesis.tar.gz | tar -xz -C $HOME/.empowerchain/config/
+# todo: curl -s https://snapshots2.nodejumper.io/empowerchain/addrbook.json > $HOME/.empowerchain/config/addrbook.json
+
+SEEDS="f2ed98cf518b501b6d1c10c4a16d0dfbc4a9cc98@tenderseed.ccvalidators.com:27001,e16668ddd526f4e114ebb6c4714f0c18c0add8f8@empower-seed.zenscape.one:26656,6740fa259552a628266a85de8c2a3dee7702b8f9@empower-mainnet-seed.itrocket.net:14656"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.empowerchain/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.empowerchain/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.empowerchain/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.empowerchain/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.empowerchain/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.001umpwr"|g' $HOME/.empowerchain/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.empowerchain/config/config.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/empowerd.service > /dev/null << EOF
+[Unit]
+Description=Empower Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which empowerd) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+empowerd tendermint unsafe-reset-all --home $HOME/.empowerchain --keep-addr-book
+
+# TODO:
+# SNAP_NAME=$(curl -s https://snapshots2.nodejumper.io/empower-testnet/info.json | jq -r .fileName)
+# curl "https://snapshots2.nodejumper.io/empowerchain/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.empowerchain"
+
+sudo systemctl daemon-reload
+sudo systemctl enable empowerd
+sudo systemctl start empowerd
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
