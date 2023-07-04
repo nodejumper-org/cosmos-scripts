0a567a08c0f5e78e3e2ccbbe099a6810dc90a6ea
Uladzislau K <uladzislau.kuzmin@gmail.com>
2023-07-04 11:23:30 +0700

    Add zetachain upgrade scripts

--- zetachain/athens_7001-1/install.sh
+++ zetachain/athens_7001-1/install.sh
@@ -9,7 +9,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="athens_7001-1"
 CHAIN_DENOM="azeta"
 BINARY_NAME="zetacored"
-BINARY_VERSION_TAG="v3.0.0"
+BINARY_VERSION_TAG="v4.0.0"
 CHEAT_SHEET="https://nodejumper.io/zetachain-testnet/cheat-sheet"
 
 printLine
