d115b9cd06dbdc14a88f32d7fdec440757e34379
vbloher <vbloher7@gmail.com>
2023-07-20 17:59:42 +0700

    add composable 4.0.1 upgrade

--- composable/banksy-testnet-3/install.sh
+++ composable/banksy-testnet-3/install.sh
@@ -9,7 +9,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="banksy-testnet-3"
 CHAIN_DENOM="ppica"
 BINARY_NAME="centaurid"
-BINARY_VERSION_TAG="v3.0.4-testnet"
+BINARY_VERSION_TAG="v4.0.1"
 CHEAT_SHEET="https://nodejumper.io/сomposable-testnet-3/cheat-sheet"
 
 printLine
@@ -28,7 +28,7 @@ cd || return
 rm -rf composable-centauri
 git clone https://github.com/notional-labs/composable-centauri
 cd composable-centauri || return
-git checkout v3.0.4-testnet
+git checkout v4.0.1
 make install
 
 centaurid config chain-id $CHAIN_ID
