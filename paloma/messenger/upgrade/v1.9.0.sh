91696701c725dcc37c62812d7134be2f40d60d73
vbloher <vbloher7@gmail.com>
2023-09-27 20:36:23 +0700

    add paloma 1.9.0 upgrade

---
+++ paloma/messenger/upgrade/v1.9.0.sh
@@ -0,0 +1,20 @@
+sudo systemctl stop pigeond
+sudo systemctl stop palomad
+
+# upgrade paloma
+cd || return
+rm -rf paloma
+git clone https://github.com/palomachain/paloma.git
+cd paloma || return
+git checkout v1.9.0
+make install
+sudo mv -f $HOME/go/bin/palomad /usr/local/bin/palomad
+
+# upgrade pigeon
+curl -L https://github.com/palomachain/pigeon/releases/download/v1.9.0/pigeon_Linux_x86_64.tar.gz > pigeon.tar.gz
+tar -xvzf pigeon.tar.gz
+rm pigeon.tar.gz
+sudo mv -f pigeon /usr/local/bin/pigeon
+
+sudo systemctl start pigeond
+sudo systemctl start palomad
