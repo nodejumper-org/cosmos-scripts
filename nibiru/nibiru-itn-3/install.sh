0be777e676dc76e2538e64920865f1297377a1b3
vbloher <vbloher7@gmail.com>
2023-10-03 22:14:38 +0700

    add nibiru itn-3 scripts

---
+++ nibiru/nibiru-itn-3/install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="nibiru-itn-3"
+CHAIN_DENOM="unibi"
+BINARY_NAME="nibid"
+BINARY_VERSION_TAG="v0.21.10"
+CHEAT_SHEET="https://nodejumper.io/nibiru-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Building binaries..." && sleep 1
+
+cd || return
+rm -rf nibiru
+git clone https://github.com/NibiruChain/nibiru
+cd nibiru || return
+git checkout v0.21.10
+make install
+
+nibid config keyring-backend test
+nibid config chain-id $CHAIN_ID
+nibid init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -s https://rpc.itn-2.nibiru.fi/genesis | jq -r .result.genesis > $HOME/.nibid/config/genesis.json
+curl -s https://snapshots-testnet.nodejumper.io/nibiru-testnet/addrbook.json > $HOME/.nibid/config/addrbook.json
+
+SEEDS="3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:13959"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.nibid/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.nibid/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001unibi"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.nibid/config/config.toml
+
+printCyan "5. Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/nibid.service > /dev/null << EOF
+[Unit]
+Description=Nibiru Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which nibid) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book
+
+SNAP_NAME=$(curl -s https://snapshots-testnet.nodejumper.io/nibiru-testnet/info.json | jq -r .fileName)
+curl "https://snapshots-testnet.nodejumper.io/nibiru-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C $HOME/.nibid
+
+sudo systemctl daemon-reload
+sudo systemctl enable nibid
+sudo systemctl start nibid
+
+printLine
+echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Check synchronization: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
+echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
---
+++ nibiru/nibiru-itn-3/upgrade/nibiru-itn-3.sh
@@ -0,0 +1,29 @@
+sudo systemctl stop nibid
+
+# build binary
+cd || return
+rm -rf nibiru
+git clone https://github.com/NibiruChain/nibiru
+cd nibiru || return
+git checkout v0.21.10
+make install
+
+# set new chain-id
+nibid config chain-id nibiru-itn-3
+
+# download new genesis
+curl -s https://rpc.itn-3.nibiru.fi/genesis | jq -r .result.genesis > $HOME/.nibid/config/genesis.json
+
+# set seed node
+SEEDS="3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:13959"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.nibid/config/config.toml
+
+# reset chain data and download fresh snapshot
+nibid tendermint unsafe-reset-all --home $HOME/.nibid
+
+SNAP_NAME=$(curl -s https://snapshots-testnet.nodejumper.io/nibiru-testnet/info.json | jq -r .fileName)
+curl "https://snapshots-testnet.nodejumper.io/nibiru-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C $HOME/.nibid
+curl -s https://snapshots-testnet.nodejumper.io/nibiru-testnet/addrbook.json > $HOME/.nibid/config/addrbook.json
+
+sudo systemctl start nibid
