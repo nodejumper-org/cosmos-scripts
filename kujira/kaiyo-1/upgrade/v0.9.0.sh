52e8db9d29973f5158562821f8ee4f1468aa37f6
vbloher <vbloher7@gmail.com>
2023-10-09 21:59:08 +0700

    add kujira 0.9.0 upgrade

---
+++ kujira/kaiyo-1/upgrade/v0.9.0.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop kujirad
+
+cd || return
+rm -rf core
+git clone https://github.com/Team-Kujira/core.git
+cd core || return
+git checkout v0.9.0
+make install
+
+sudo systemctl start kujirad
