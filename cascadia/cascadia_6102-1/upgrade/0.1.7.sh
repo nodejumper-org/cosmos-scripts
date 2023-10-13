f193929be68474172abf3a5aed614476799e465a
vbloher <vbloher7@gmail.com>
2023-10-13 15:22:09 +0700

    add cascadia upgrade instructions

---
+++ cascadia/cascadia_6102-1/upgrade/0.1.7.sh
@@ -0,0 +1,7 @@
+sudo systemctl stop cascadiad
+
+curl -L https://github.com/CascadiaFoundation/cascadia/releases/download/v0.1.7/cascadiad -o cascadiad
+chmod +x cascadiad
+sudo mv cascadiad /usr/local/bin
+
+sudo systemctl start cascadiad
