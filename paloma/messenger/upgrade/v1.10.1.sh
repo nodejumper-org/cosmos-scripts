e162bea169c41c06a903393b233abb1d9bc046ee
vbloher <vbloher7@gmail.com>
2023-12-02 10:17:59 +0700

    add pigeon to paloma upgrade v1.10.1

--- paloma/messenger/upgrade/v1.10.1.sh
+++ paloma/messenger/upgrade/v1.10.1.sh
@@ -10,5 +10,11 @@ git checkout v1.10.1
 make install
 sudo mv -f $HOME/go/bin/palomad /usr/local/bin/palomad
 
+# upgrade pigeon
+curl -L https://github.com/palomachain/pigeon/releases/download/v1.10.1/pigeon_Linux_x86_64.tar.gz > pigeon.tar.gz
+tar -xvzf pigeon.tar.gz
+rm pigeon.tar.gz
+sudo mv -f pigeon /usr/local/bin/pigeon
+
 sudo systemctl start pigeond
 sudo systemctl start palomad
