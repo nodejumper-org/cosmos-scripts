a97acd75de50f66285a74160eb9c0f23e3bed9b5
vbloher <vbloher7@gmail.com>
2023-12-06 13:48:27 +0700

    add peers to cascadia upgrade

--- cascadia/cascadia_11029-1/upgrade/cascadia_11029-1.sh
+++ cascadia/cascadia_11029-1/upgrade/cascadia_11029-1.sh
@@ -11,6 +11,11 @@ sudo mv cascadiad /usr/local/bin
 # update genesis
 curl -# -L https://raw.githubusercontent.com/CascadiaFoundation/chain-configuration/master/testnet/genesis.json -o ~/.cascadiad/config/genesis.json
 
+# update peers
+SEEDS=""
+PEERS="d1ed80e232fc2f3742637daacab454e345bbe475@54.204.246.120:26656,0c96a6c328eb58d1467afff4130ab446c294108c@34.239.67.55:26656"
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.cascadiad/config/config.toml
+
 # update service file
 sudo tee /etc/systemd/system/cascadiad.service > /dev/null << EOF
 [Unit]
@@ -27,7 +32,7 @@ WantedBy=multi-user.target
 EOF
 
 # reset all chain data
-cascadiad tendermint unsafe-reset-all --home $HOME/.cascadiad --keep-addr-book
+cascadiad tendermint unsafe-reset-all --home $HOME/.cascadiad
 
 sudo systemctl daemon-reload
 sudo systemctl restart cascadiad
