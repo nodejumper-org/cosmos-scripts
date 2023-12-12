ad67d2087d97fc7e37717b0552165ded4235bfab
vbloher <vbloher7@gmail.com>
2023-12-12 18:24:31 +0700

    update dymension version

--- dymension/froopyland_100-1/upgrade/v2.sh
+++ dymension/froopyland_100-1/upgrade/v2.sh
@@ -4,7 +4,7 @@ cd || return
 rm -rf dymension
 git clone https://github.com/dymensionxyz/dymension.git
 cd dymension || return
-git checkout v2.0.0-alpha.4
+git checkout v2.0.0-alpha.5
 make install
 
 sudo systemctl start dymd
