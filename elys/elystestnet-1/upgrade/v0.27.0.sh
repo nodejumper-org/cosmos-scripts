7db9b639ff29d0ec8277476b4ae4c821b72dfa4b
vbloher <vbloher7@gmail.com>
2024-01-23 18:02:13 +0700

    update elys 0.27 upgrade script

--- elys/elystestnet-1/upgrade/v0.27.0.sh
+++ elys/elystestnet-1/upgrade/v0.27.0.sh
@@ -3,7 +3,9 @@ sudo systemctl stop elysd
 cd && rm -rf elys
 git clone https://github.com/elys-network/elys
 cd elys
-git checkout v0.27.0
+git checkout fix/missing-margin-migrator
+git tag v0.27.0 -d
+git tag v0.27.0
 make install
 
 sudo systemctl start elysd
