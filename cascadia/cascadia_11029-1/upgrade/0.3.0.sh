809e6b11511ea9cf70bd54b997319f17c17438ae
vbloher <vbloher7@gmail.com>
2024-01-24 14:02:10 +0700

    add cascadia 0.3.0 upgrade

---
+++ cascadia/cascadia_11029-1/upgrade/0.3.0.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop cascadiad
+
+
+cd && rm -rf cascadia
+git clone https://github.com/CascadiaFoundation/cascadia
+cd cascadia
+git checkout v0.3.0
+make install
+
+sudo systemctl start cascadiad
