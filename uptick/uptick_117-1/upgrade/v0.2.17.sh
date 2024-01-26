f2e159e2463a3ffa7507e6d035af07174e67e008
vbloher <vbloher7@gmail.com>
2024-01-26 11:48:14 +0700

    add uptick 0.2.17 upgrade

---
+++ uptick/uptick_117-1/upgrade/v0.2.17.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop uptickd
+
+cd $HOME && rm -rf uptick
+git clone https://github.com/UptickNetwork/uptick.git
+cd uptick || return
+git checkout v0.2.17
+make build -B
+sudo mv build/uptickd $(which uptickd)
+
+sudo systemctl start uptickd
