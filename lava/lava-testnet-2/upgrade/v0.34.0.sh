69e3e64fdc8b69d15e5bbfa99fe65640a3e2702d
Uladzislau K <uladzislau.kuzmin@gmail.com>
2024-01-31 01:32:56 +0700

    Add lava upgrade scripts

---
+++ lava/lava-testnet-2/upgrade/v0.34.0.sh
@@ -0,0 +1,12 @@
+sudo systemctl stop lavad
+
+export LAVA_BINARY=lavad
+
+cd || return
+rm -rf lava
+git clone https://github.com/lavanet/lava
+cd lava || return
+git checkout v0.34.0
+make install
+
+sudo systemctl start lavad
