a702ea60c94dd243b96e31ab6f11447511d35ca2
vbloher <vbloher7@gmail.com>
2024-02-01 21:05:41 +0700

    add soarchain

---
+++ soarchain/iov-mainnet-ibc/custom_scripts/build_binary_step.sh
@@ -0,0 +1,9 @@
+# Download binary
+cd $HOME
+curl -s https://raw.githubusercontent.com/soar-robotics/testnet-binaries/main/v0.2.6/ubuntu22.04/soarchaind > soarchaind
+chmod +x soarchaind
+mkdir -p $HOME/go/bin/
+mv soarchaind $HOME/go/bin/
+
+# Install libwasmvm
+curl -L https://snapshots-testnet.nodejumper.io/soarchain-testnet/libwasmvm.x86_64.so > /var/lib/libwasmvm.x86_64.so
