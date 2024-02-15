23d0841cbac5312329ee0c88936df89711cd8ad4
vbloher <vbloher7@gmail.com>
2024-02-15 15:43:13 +0700

    add sei pacific-1 instructions

---
+++ sei/pacific-1/install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Clone project repository
+cd && rm -rf sei-chain
+git clone https://github.com/sei-protocol/sei-chain.git
+cd sei-chain
+git checkout 3.6.1
+
+# Build binary
+make install
+
+# Set node CLI configuration
+seid config chain-id pacific-1
+seid config keyring-backend file
+
+# Initialize the node
+seid init "YOUR_NODE_NAME_HERE" --chain-id pacific-1
+
+# Download genesis
+curl -s https://raw.githubusercontent.com/sei-protocol/testnet/main/pacific-1/genesis.json > $HOME/.sei/config/genesis.json
+
+# Set seeds
+SEEDS="400f3d9e30b69e78a7fb891f60d76fa3c73f0ecc@sei.rpc.kjnodes.com:16859,20e1000e88125698264454a884812746c2eb4807@seeds.lavenderfive.com:11956,ebc272824924ea1a27ea3183dd0b9ba713494f83@sei-mainnet-seed.autostake.com:26806,c28827cb96c14c905b127b92065a3fb4cd77d7f6@seeds.whispernode.com:11956"
+sed -i 's|^bootstrap-peers *=.*|bootstrap-peers = "'$SEEDS'"|' $HOME/.sei/config/config.toml
+
+# Set minimum-gas-prices
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.1usei"|g' $HOME/.sei/config/app.toml
+
+# Enable RPC
+sed -i \
+  -e 's|laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://0.0.0.0:26657"|g' \
+  -e 's|^cors_allowed_origins *=.*|cors_allowed_origins = ["*"]|g' $HOME/.sei/config/config.toml
+
+# Create a service
+sudo tee /etc/systemd/system/seid.service > /dev/null << EOF
+[Unit]
+Description=Sei Protocol Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which seid) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable seid
+
+# Download latest chain data snapshot
+# go to the Polkachu's website and download the latest snapshot
+# https://polkachu.com/tendermint_snapshots/sei
+
+# Start the service
+sudo systemctl start seid
