ce1d8cfefa14ca3a09ae22b4265265426fa4ebff
vbloher <vbloher7@gmail.com>
2024-02-21 23:03:07 +0700

    fox entrypoint build step

--- entrypoint/entrypoint-pubtest-2/custom_scripts/build_binary_step.sh
+++ entrypoint/entrypoint-pubtest-2/custom_scripts/build_binary_step.sh
@@ -1,6 +1,6 @@
 # Download binary
 cd $HOME
 curl -s https://github.com/entrypoint-zone/testnets/releases/download/v${tag}/entrypointd-${tag}-linux-amd64 > entrypointd
-chmod +x soarchaind
+chmod +x entrypointd
 mkdir -p $HOME/go/bin/
 mv entrypointd $HOME/go/bin/
