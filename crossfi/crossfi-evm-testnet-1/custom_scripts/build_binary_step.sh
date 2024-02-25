3e636f9b28ca3b194bb1408f9a5b4e1ef8437fef
vbloher <vbloher7@gmail.com>
2024-02-25 14:05:20 +0700

    remove readme also

--- crossfi/crossfi-evm-testnet-1/custom_scripts/build_binary_step.sh
+++ crossfi/crossfi-evm-testnet-1/custom_scripts/build_binary_step.sh
@@ -4,4 +4,4 @@ curl -L https://github.com/crossfichain/crossfi-node/releases/download/v${tag}/c
 tar -xvzf crossfi-node_${tag}_linux_amd64.tar.gz
 chmod +x $HOME/bin/crossfid
 mv $HOME/bin/crossfid $HOME/go/bin
-rm -rf crossfi-node_${tag}_linux_amd64.tar.gz $HOME/bin
+rm -rf crossfi-node_${tag}_linux_amd64.tar.gz readme.md $HOME/bin
