bbbbf2b1cc834598765365bd2f1f81b5ce3e0ce8
vbloher <vbloher7@gmail.com>
2024-02-26 16:11:48 +0700

    use signet network for babylon

--- babylon/bbn-test-3/custom_scripts/additional_chain_config_step.sh
+++ babylon/bbn-test-3/custom_scripts/additional_chain_config_step.sh
@@ -1,2 +1,2 @@
 # Set additional configs
-sed -i 's|^network *=.*|network = "mainnet"|g' $HOME/.babylond/config/app.toml
+sed -i 's|^network *=.*|network = "signet"|g' $HOME/.babylond/config/app.toml
