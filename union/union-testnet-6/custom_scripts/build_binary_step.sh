85df70a9ff2fa2b9fe57d2a7c605533ada7e9316
vbloher <vbloher7@gmail.com>
2024-02-29 20:14:42 +0700

    do not use sudo for chmod

--- union/union-testnet-6/custom_scripts/build_binary_step.sh
+++ union/union-testnet-6/custom_scripts/build_binary_step.sh
@@ -2,4 +2,4 @@
 cd $HOME
 mkdir -p $HOME/go/bin
 curl -L https://snapshots-testnet.nodejumper.io/union-testnet/uniond-${tag}-linux-amd64 > $HOME/go/bin/uniond
-sudo chmod +x $HOME/go/bin/uniond
+chmod +x $HOME/go/bin/uniond
