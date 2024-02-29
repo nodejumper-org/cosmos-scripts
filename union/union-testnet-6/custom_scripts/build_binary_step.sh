a0fc0478c2f0b529dbd4bf50f7197b7d15f17daa
vbloher <vbloher7@gmail.com>
2024-02-29 14:37:32 +0700

    add tag for union binary

--- union/union-testnet-6/custom_scripts/build_binary_step.sh
+++ union/union-testnet-6/custom_scripts/build_binary_step.sh
@@ -1,4 +1,5 @@
 # Download binary
-cd $HOME && mkdir -p $HOME/go/bin
-curl -L https://snapshots-testnet.nodejumper.io/union-testnet/uniond-0.20.0-linux-amd64 > $HOME/go/bin/uniond
+cd $HOME
+mkdir -p $HOME/go/bin
+curl -L https://snapshots-testnet.nodejumper.io/union-testnet/uniond-${tag}-linux-amd64 > $HOME/go/bin/uniond
 sudo chmod +x $HOME/go/bin/uniond
