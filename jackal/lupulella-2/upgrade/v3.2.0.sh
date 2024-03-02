a447e5b4fcb7ce6bcf9785cbda4f0fd4a1b25789
vbloher <vbloher7@gmail.com>
2024-03-02 11:55:11 +0700

    remove extra upgrade files

--- jackal/lupulella-2/upgrade/v3.2.0.sh
+++
@@ -1,9 +0,0 @@
-sudo systemctl stop canined
-
-cd && rm -rf canine-chain
-git clone https://github.com/JackalLabs/canine-chain.git
-cd canine-chain || return
-git checkout v3.2.0
-make install
-
-sudo systemctl start canined
