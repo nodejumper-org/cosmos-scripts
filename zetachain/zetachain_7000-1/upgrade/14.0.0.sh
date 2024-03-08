45f701e82b296bfc4543f42c8700b5b1cb812a04
vbloher <vbloher7@gmail.com>
2024-03-08 08:54:30 +0700

    upgrade zetachain

--- zetachain/zetachain_7000-1/upgrade/14.0.0.sh
+++ zetachain/zetachain_7000-1/upgrade/14.0.0.sh
@@ -3,7 +3,7 @@ sudo systemctl stop zetacored
 cd && rm -rf node
 git clone https://github.com/zeta-chain/node
 cd node
-git checkout v14.0.0
+git checkout v14.0.1
 make install
 
 sudo systemctl start zetacored
