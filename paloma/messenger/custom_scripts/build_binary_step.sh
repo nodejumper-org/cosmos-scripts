4004a7aef4f8f437c9ddf9e275808ce7353cc291
vbloher <vbloher7@gmail.com>
2024-04-16 20:40:27 +0700

    update libwasm version

--- paloma/messenger/custom_scripts/build_binary_step.sh
+++ paloma/messenger/custom_scripts/build_binary_step.sh
@@ -6,7 +6,7 @@ cd paloma
 git checkout ${tag}
 
 # Install libwasmvm
-curl -L https://github.com/CosmWasm/wasmvm/releases/download/v1.4.0/libwasmvm.x86_64.so > libwasmvm.x86_64.so
+curl -L https://github.com/CosmWasm/wasmvm/releases/download/v1.5.0/libwasmvm.x86_64.so > libwasmvm.x86_64.so
 sudo mv -f libwasmvm.x86_64.so /usr/lib/libwasmvm.x86_64.so
 
 # Build binary
