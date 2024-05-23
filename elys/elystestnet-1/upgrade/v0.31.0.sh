fe949ddb73041b71fe14ebbc27a03508e57f1b02
vbloher <vbloher7@gmail.com>
2024-05-23 09:27:29 +0300

    update elys v0.31.0 upgrde script

--- elys/elystestnet-1/upgrade/v0.31.0.sh
+++ elys/elystestnet-1/upgrade/v0.31.0.sh
@@ -1,7 +1,8 @@
 cd && rm -rf elys
 git clone https://github.com/elys-network/elys
 cd elys
-git checkout v0.31.0
+git checkout fix/v0.31.0-increase-max-block-size-disable-allocateTokens
+git tag -f v0.31.0
 make install
 
 sudo systemctl restart elysd
