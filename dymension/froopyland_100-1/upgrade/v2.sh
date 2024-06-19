35bb3830d52ed86a844b95070f7d2eda0cb184fa
vbloher <vbloher7@gmail.com>
2024-06-19 21:32:26 +0300

    update dymension-testnet v2 upgrade

--- dymension/froopyland_100-1/upgrade/v2.sh
+++ dymension/froopyland_100-1/upgrade/v2.sh
@@ -1,10 +1,12 @@
-sudo systemctl stop dymd
-
-cd || return
-rm -rf dymension
+cd && rm -rf dymension
 git clone https://github.com/dymensionxyz/dymension.git
-cd dymension || return
+cd dymension
 git checkout v2.0.0-alpha.7
 make install
+
+sudo systemctl stop dymd
+
+# fix state before start
+dymd hotfix
 
 sudo systemctl start dymd
