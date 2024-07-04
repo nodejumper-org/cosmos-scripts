97249b1972d0dc30da36a4a1ef3680bd19919660
vbloher <vbloher7@gmail.com>
2024-07-04 14:53:34 +0300

    add new artela upgrade script

---
+++ artela/artela_11820-1/upgrade/v0.4.7-rc7-fix-execution.sh
@@ -0,0 +1,44 @@
+# build new binary
+cd && rm -rf artela
+git clone https://github.com/artela-network/artela
+cd artela
+git checkout v0.4.7-rc7-fix-execution
+make install
+
+# download aspect lib
+mkdir -p $HOME/.artelad/libs && cd $HOME/.artelad/libs
+curl -L https://github.com/artela-network/artela/releases/download/v0.4.7-rc7/artelad_0.4.7_rc7_Linux_amd64.tar.gz -o artelad_0.4.7_rc7_Linux_amd64.tar.gz
+tar -xvzf artelad_0.4.7_rc7_Linux_amd64.tar.gz
+rm artelad_0.4.7_rc7_Linux_amd64.tar.gz
+
+# update systemd service to use lib path
+sudo systemctl stop artelad
+
+# update addrbook
+curl -s https://snapshots-testnet.nodejumper.io/artela-testnet/addrbook.json > $HOME/.artelad/config/addrbook.json
+
+# download a snapshot
+cp $HOME/.artelad/data/priv_validator_state.json $HOME/.artelad/priv_validator_state.json.backup
+artelad tendermint unsafe-reset-all --home $HOME/.artelad --keep-addr-book
+curl https://snapshots-testnet.nodejumper.io/artela-testnet/artela-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.artelad
+mv $HOME/.artelad/priv_validator_state.json.backup $HOME/.artelad/data/priv_validator_state.json
+
+# update service file
+sudo tee /etc/systemd/system/artelad.service > /dev/null << EOF
+[Unit]
+Description=Artela node service
+After=network-online.target
+[Service]
+User=$USER
+Environment="LD_LIBRARY_PATH=$HOME/.artelad/libs:\$LD_LIBRARY_PATH"
+ExecStart=$(which artelad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+
+# start the service
+sudo systemctl start artelad
