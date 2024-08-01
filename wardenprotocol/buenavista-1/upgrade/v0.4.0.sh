fade0ba146119f61b463d04dce928cf9be8acd0b
vbloher <vbloher7@gmail.com>
2024-08-01 15:47:22 +0300

    fix warden home path

--- wardenprotocol/buenavista-1/upgrade/v0.4.0.sh
+++ wardenprotocol/buenavista-1/upgrade/v0.4.0.sh
@@ -3,7 +3,7 @@
 sudo systemctl stop wardend
 
 # Backup a validator state
-cp $HOME/.wardend/data/priv_validator_state.json $HOME/priv_validator_state_WARDEN.json
+cp $HOME/.warden/data/priv_validator_state.json $HOME/priv_validator_state_WARDEN.json
 
 # Download and install the new binary
 cd && wget https://github.com/warden-protocol/wardenprotocol/releases/download/v0.4.0/wardend_Linux_x86_64.zip
