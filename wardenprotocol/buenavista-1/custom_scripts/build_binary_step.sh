e25def0ba53187172deaa571239ef2cb8e6a9129
vbloher <vbloher7@gmail.com>
2024-08-04 01:25:18 +0300

    fix warden install script

--- wardenprotocol/buenavista-1/custom_scripts/build_binary_step.sh
+++ wardenprotocol/buenavista-1/custom_scripts/build_binary_step.sh
@@ -1,8 +1,6 @@
-# Clone project repository
-cd $HOME && rm -rf wardenprotocol
-git clone https://github.com/warden-protocol/wardenprotocol
-cd  wardenprotocol
-git checkout ${tag}
-
-# Build binary
-make install-wardend
+# Download binary
+cd && wget https://github.com/warden-protocol/wardenprotocol/releases/download/${tag}/wardend_Linux_x86_64.zip
+unzip wardend_Linux_x86_64.zip
+rm -rf wardend_Linux_x86_64.zip
+chmod +x wardend
+sudo mv wardend $(which wardend)
