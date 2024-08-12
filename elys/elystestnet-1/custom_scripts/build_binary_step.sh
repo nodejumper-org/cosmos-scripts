106e3455d953aefd81bf447c80ffbf4b7b2601a6
vbloher <vbloher7@gmail.com>
2024-08-12 11:50:00 +0300

    remove custom build step for elys

--- elys/elystestnet-1/custom_scripts/build_binary_step.sh
+++
@@ -1,8 +0,0 @@
-# Clone project repository
-cd && rm -rf elys
-git clone https://github.com/elys-network/elys
-cd elys
-git checkout ${tag}
-
-# Build binary
-ROCKSDB=1 LD_LIBRARY_PATH=/usr/local/lib make install
