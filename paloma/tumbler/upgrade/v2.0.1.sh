bf186bfcc338a4b092d49d60a0dec1798473f858
vbloher <vbloher7@gmail.com>
2024-08-21 10:43:49 +0300

    add paloma upgrade

---
+++ paloma/tumbler/upgrade/v2.0.1.sh
@@ -0,0 +1,7 @@
+cd && rm -rf paloma
+git clone -b v2.0.1 https://github.com/palomachain/paloma.git
+cd paloma
+make build
+sudo mv -f build/palomad "$(which palomad)"
+
+sudo systemctl restart palomad
