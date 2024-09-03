9d0f9150b1f121e47a97176bba4aa7fa4ba8d2de
vbloher <vbloher7@gmail.com>
2024-09-03 14:00:30 +0300

    add paloma upgrade

--- paloma/tumbler/upgrade/v2.1.2.sh
+++ paloma/tumbler/upgrade/v2.1.2.sh
@@ -1,16 +1,8 @@
 # upgrade paloma
 cd && rm -rf paloma
-git clone -b v2.1.0 https://github.com/palomachain/paloma.git
+git clone -b v2.1.2 https://github.com/palomachain/paloma.git
 cd paloma
 make build
 sudo mv -f build/palomad "$(which palomad)"
 
-# upgrade pigeon
-cd && rm -rf pigeon
-git clone -b v2.1.0 https://github.com/palomachain/pigeon
-cd pigeon
-make build
-sudo mv -f build/pigeon "$(which pigeon)"
-
-sudo systemctl restart pigeond
 sudo systemctl restart palomad
