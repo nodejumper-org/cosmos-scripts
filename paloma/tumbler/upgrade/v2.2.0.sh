0b834ff5b45a9a36429346b650298501c6a54933
vbloher <vbloher7@gmail.com>
2024-09-07 14:18:53 +0300

    add pigeon upgrade

--- paloma/tumbler/upgrade/v2.2.0.sh
+++ paloma/tumbler/upgrade/v2.2.0.sh
@@ -5,4 +5,12 @@ cd paloma
 make build
 sudo mv -f build/palomad "$(which palomad)"
 
+# upgrade pigeon
+cd && rm -rf pigeon
+git clone -b v2.2.0 https://github.com/palomachain/pigeon
+cd pigeon
+make build
+sudo mv -f build/pigeon "$(which pigeon)"
+
+sudo systemctl restart pigeond
 sudo systemctl restart palomad
