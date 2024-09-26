aea0033ddede0db88a6a5783e4fd3a4d4afce6b9
vbloher <vbloher7@gmail.com>
2024-09-26 12:29:23 +0300

    fix warden script

--- warden/buenavista-1/custom_scripts/build_binary_step.sh
+++ warden/buenavista-1/custom_scripts/build_binary_step.sh
@@ -3,4 +3,4 @@ cd && wget https://github.com/warden-protocol/wardenprotocol/releases/download/$
 unzip wardend_Linux_x86_64.zip
 rm -rf wardend_Linux_x86_64.zip
 chmod +x wardend
-sudo mv wardend $(which wardend)
+sudo mv wardend $HOME/go/bin/warndend
