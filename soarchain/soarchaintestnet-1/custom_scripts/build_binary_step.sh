767919a061ed32d04f0b7452a77b5f850205ef3b
vbloher <vbloher7@gmail.com>
2024-10-16 14:01:41 +0300

    fix soarchain script

--- soarchain/soarchaintestnet-1/custom_scripts/build_binary_step.sh
+++ soarchain/soarchaintestnet-1/custom_scripts/build_binary_step.sh
@@ -1,10 +1,10 @@
 # Download binary
 cd $HOME
-curl -s https://raw.githubusercontent.com/soar-robotics/testnet-binaries/main/${tag}/ubuntu22.04/soarchaind > soarchaind
+curl -s https://snapshots-testnet.nodejumper.io/soarchain/soarchaind > soarchaind
 chmod +x soarchaind
 mkdir -p $HOME/go/bin/
 mv soarchaind $HOME/go/bin/
 
 # Install libwasmvm
-curl -L https://snapshots-testnet.nodejumper.io/soarchain-testnet/libwasmvm.x86_64.so > libwasmvm.x86_64.so
+curl -L https://snapshots-testnet.nodejumper.io/soarchain/libwasmvm.x86_64.so > libwasmvm.x86_64.so
 sudo mv libwasmvm.x86_64.so /var/lib/libwasmvm.x86_64.so
