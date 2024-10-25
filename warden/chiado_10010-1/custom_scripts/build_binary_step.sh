dcdfc8dee4fc6939f64b2fbd73ea492501390e4d
vbloher <vbloher7@gmail.com>
2024-10-25 11:04:40 +0300

    add new warden testnet

---
+++ warden/chiado_10010-1/custom_scripts/build_binary_step.sh
@@ -0,0 +1,6 @@
+# Download binary
+cd && wget https://github.com/warden-protocol/wardenprotocol/releases/download/${tag}/wardend_Linux_x86_64.zip
+unzip wardend_Linux_x86_64.zip
+rm -rf wardend_Linux_x86_64.zip
+chmod +x wardend
+sudo mv wardend $HOME/go/bin/warndend
